6525016bc3fb6419cb62abbc5f1a96511220b1f6
Brendan Kennedy <brendan.kennedy@skyspecs.com>
2019-12-17 17:51:41 -0500

    Conversion work

--- tests/server.js
+++ tests/server.js
@@ -1,22 +1,12 @@
 const fs = require('fs-extra');
 const express = require('express');
 const convert = require('../lib/convert');
+const download = require('../lib/download');
 
 const app = express();
 const port = 4000;
 
 let isProcessing = false;
-
-try {
-  fs.mkdirSync('./results');
-} catch (error) {}
-
-const resultPath = './results/out.json';
-
-const runVideoTest = async () => {
-  const res = await convert();
-  await fs.writeFile(resultPath, JSON.stringify(res, null, 2));
-};
 
 app.get('/', (req, res) => {
   res.status(200).send({
@@ -24,7 +14,11 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/test', async (req, res) => {
+app.get('/convert', async (req, res) => {
+  if (!req.query.url) {
+    return res.status(400).send({ message: 'invalid url' });
+  }
+
   if (isProcessing) {
     res.status(429).send({
       message: 'server busy',
@@ -35,10 +29,30 @@ app.get('/test', async (req, res) => {
       message: 'started running tests',
     });
 
-    console.log('Running video test');
-    await runVideoTest();
+    console.log('Converting video');
+    const videoPath = await download(req.query.url);
+    const conversion = await new convert(videoPath)
+      .add('-y')
+      .add('-progress -')
+      .add('-c:v libx264')
+      .add('-preset veryfast')
+      .add('-profile:v high')
+      .add('-crf 22')
+      .add('-coder 1')
+      .add('-pix_fmt yuv420p')
+      .add('-movflags +faststart')
+      .add('-bf 2')
+      .add('-c:a aac')
+      .add('-ac 2')
+      .add('-b:a 192K')
+      .add('-ar 48000')
+      .add('-profile:a aac_low')
+      .add('./data/export/legend-convert.mp4')
+      .process();
+
+    console.log('Conversion Complete', conversion);
     isProcessing = false;
-    console.log('is processing');
+    console.log(`is processing? : ${isProcessing}`);
   }
 });
 
