1c2dd07352aa3e02e0956e6ba861e494b19e029a
Brendan Kennedy <brenwken@gmail.com>
2019-12-31 00:01:14 -0500

    Fix terminate script curl request

--- scripts/terminate.sh
+++ scripts/terminate.sh
@@ -4,4 +4,4 @@ echo "Terminating Server"
 DROPLET_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
 API_KEY=$1
 
-curl -X DELETE -H 'Content-Type: application/json' -H "Authorization: Bearer $API_KEY" "https://api.digitalocean.com/v2/droplets/$DROPLET_ID)"
+curl -X DELETE -H 'Content-Type: application/json' -H "Authorization: Bearer $API_KEY" "https://api.digitalocean.com/v2/droplets/$DROPLET_ID"
