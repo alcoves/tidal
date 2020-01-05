73caea4eec1888d7b2e17c08dad4c4d0555b8f19
Brendan Kennedy <brenwken@gmail.com>
2020-01-05 03:56:39 -0500

    Allow for more http retries

--- config/s3.js
+++ config/s3.js
@@ -4,6 +4,11 @@ const WASABI_ENDPOINT = 'https://s3.us-east-2.wasabisys.com';
 AWS.config.update({
   accessKeyId: process.env.WASABI_ACCESS_KEY_ID,
   secretAccessKey: process.env.WASABI_SECRET_ACCESS_KEY,
+  maxRetries: 6,
+  httpOptions: {
+    timeout: 30000,
+    connectTimeout: 5000,
+  },
 });
 
 const s3 = new AWS.S3({
