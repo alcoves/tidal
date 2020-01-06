b0cf542aca670e39d32740dbbb782f6dbe10de46
Brendan Kennedy <brenwken@gmail.com>
2020-01-05 21:55:52 -0500

    Wasabi timeout debugging

--- config/s3.js
+++ config/s3.js
@@ -1,20 +1,17 @@
 const AWS = require('aws-sdk');
-const WASABI_ENDPOINT = 'https://s3.us-east-2.wasabisys.com';
 
 AWS.config.update({
   accessKeyId: process.env.WASABI_ACCESS_KEY_ID,
   secretAccessKey: process.env.WASABI_SECRET_ACCESS_KEY,
-  maxRetries: 6,
+  maxRetries: 2,
   httpOptions: {
-    timeout: 30000,
-    connectTimeout: 5000,
+    timeout: 5000,
+    connectTimeout: 3000,
   },
 });
 
 const s3 = new AWS.S3({
-  endpoint: new AWS.Endpoint(WASABI_ENDPOINT),
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4',
+  endpoint: new AWS.Endpoint('s3.us-east-2.wasabisys.com'),
 });
 
 module.exports = s3;
