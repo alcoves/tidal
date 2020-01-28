09e5292dc3586118cbf5623a3b659dec6e43733e
Brendan Kennedy <brenwken@gmail.com>
2020-01-28 00:53:20 -0500

    Update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
 # This script is used to create a new base image
 
+# Update deps
 apt update
-apt install -y git nodejs npm ffmpeg
-npm i -g yarn
+
+# Install deps
+apt install -y \
+  software-properties-common \
+  nginx \
+  git \
+  htop \
+  vnstat \
+  wget \
+  curl \
+  sudo \
+  ffmpeg
+
+# Install node deps
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install -y nodejs
+npm i -g yarn pm2
+
 cd /root
 git clone https://github.com/bken-io/video
 cd video
 yarn
 
-# Environment variables are injected at runtime
+# Download secrets and store them on disk
+# .env
