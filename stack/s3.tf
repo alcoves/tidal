58189f509bce7e5e1b1c2643cc249e8496100512
Brendan Kennedy <brenwken@gmail.com>
2020-03-02 00:48:04 -0500

    Fix s3 notifications

--- stack/s3.tf
+++ stack/s3.tf
@@ -32,17 +32,6 @@ resource "aws_lambda_permission" "allow_seg_lambda_access" {
   function_name = module.tidal_dev_segmentation_spawner.arn
 }
 
-resource "aws_s3_bucket_notification" "tidal_bken_dev_source_upload_event" {
-  bucket     = aws_s3_bucket.tidal_bken_dev.id
-  depends_on = [aws_lambda_permission.allow_seg_lambda_access]
-
-  lambda_function {
-    filter_prefix       = "sources/"
-    events              = ["s3:ObjectCreated:*"]
-    lambda_function_arn = module.tidal_dev_segmentation_spawner.arn
-  }
-}
-
 resource "aws_lambda_permission" "allow_transcode_enqueuer_access" {
   principal     = "s3.amazonaws.com"
   action        = "lambda:InvokeFunction"
@@ -51,9 +40,19 @@ resource "aws_lambda_permission" "allow_transcode_enqueuer_access" {
   function_name = module.tidal_dev_transcode_enqueuer.arn
 }
 
-resource "aws_s3_bucket_notification" "tidal_bken_dev_metadata_create_event" {
-  bucket     = aws_s3_bucket.tidal_bken_dev.id
-  depends_on = [aws_lambda_permission.allow_seg_lambda_access]
+resource "aws_s3_bucket_notification" "tidal_bken_dev_event_mapping" {
+  bucket = aws_s3_bucket.tidal_bken_dev.id
+
+  depends_on = [
+    aws_lambda_permission.allow_seg_lambda_access,
+    aws_lambda_permission.allow_transcode_enqueuer_access
+  ]
+
+  lambda_function {
+    filter_prefix       = "sources/"
+    events              = ["s3:ObjectCreated:*"]
+    lambda_function_arn = module.tidal_dev_segmentation_spawner.arn
+  }
 
   lambda_function {
     filter_prefix       = "metadata/"
