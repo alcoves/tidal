91c46e11ca29a95e414fc43da4cbc33b31df43c5
Brendan Kennedy <brenwken@gmail.com>
2020-03-12 00:52:21 -0400

    Remove javascript src

--- src/lib/mkTmpDir.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs-extra');
-const shortid = require('shortid');
-
-module.exports = () => {
-  fs.mkdirpSync('./tmp');
-  return fs.mkdirpSync(path.resolve(`./tmp/${shortid()}`));
-};
