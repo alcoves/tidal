dc76ca323f6b467b6fe2766c7955311436f236bb
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 05:24:46 -0400

    Transcode

--- scripts/thumbnailer.sh
+++ scripts/thumbnailer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-FILE_NAME=local/file
-THUMB_NAME=local/thumb.webp
+VIDEO_INPUT_PATH=local/file
+THUMBNAIL_OUTPUT_PATH=local/thumb.webp
 
-# wget -O $FILE_NAME $1
-ffmpeg -i $FILE_NAME -vframes 1 -ss $2 -filter:v scale='480:-1' -quality 70 $THUMB_NAME
-aws s3 cp $THUMB_NAME s3://bken-tidal-dev/test/$THUMB_NAME
+ffmpeg -y -i $VIDEO_INPUT_PATH -vframes 1 -ss $2 -filter:v scale='480:-1' -quality 70 $THUMBNAIL_OUTPUT_PATH
+
+aws s3 cp $THUMBNAIL_OUTPUT_PATH s3://bken-sandbox-dev/thumb.jpg
