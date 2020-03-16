9087fe5682a967e2212a2de6fe962d20b64864bc
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 07:33:17 -0400

    debug

--- scripts/thumbnailer.sh
+++ scripts/thumbnailer.sh
@@ -6,4 +6,7 @@ THUMBNAIL_OUTPUT_PATH=local/thumb.webp
 
 ffmpeg -y -i $VIDEO_INPUT_PATH -vframes 1 -ss $2 -filter:v scale='480:-1' -quality 70 $THUMBNAIL_OUTPUT_PATH
 
+echo "Done processing thumbnail"
+ls local/
+
 s3cmd put -c local/s3cfg.ini $THUMBNAIL_OUTPUT_PATH s3://bken-sandbox-dev/thumb.jpg
