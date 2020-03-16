a786e20a7ea397c6144a3b9b8806f8dad15f24d7
Brendan Kennedy <brenwken@gmail.com>
2020-03-15 23:27:06 -0400

    Transcode

--- scripts/transcode.sh
+++ scripts/transcode.sh
@@ -5,4 +5,4 @@ TMP_DIR=$(mktemp -d)
 
 wget -O "${TMP_DIR}/source.mp4" $1
 
-ffmpeg "${TMP_DIR}/source.mp4" -c copy "${TMP_DIR}/outFile.mp4"
+ffmpeg -i "${TMP_DIR}/source.mp4" -c copy "${TMP_DIR}/outFile.mp4"
