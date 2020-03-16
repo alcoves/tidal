81131c8c071af39fe7da8dc79a5ea181955c22c4
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 07:21:31 -0400

    add s3Cmd ini

--- scripts/transcode.sh
+++ scripts/transcode.sh
@@ -6,4 +6,4 @@ VIDEO_OUTPUT_PATH=local/out.mp4
 
 ffmpeg -y -i $VIDEO_INPUT_PATH -c:v libx264 -crf 22 -preset fast $VIDEO_OUTPUT_PATH
 
-s3cmd mv $VIDEO_OUTPUT_PATH s3://bken-sandbox-dev/720p-converted.mp4
+s3cmd mv -c local/s3cfg.ini $VIDEO_OUTPUT_PATH s3://bken-sandbox-dev/720p-converted.mp4
