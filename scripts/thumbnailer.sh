7619b9402152a00ef5b7f1dc37bfa4e70a84b45e
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 07:03:28 -0400

    Add https

--- scripts/thumbnailer.sh
+++ scripts/thumbnailer.sh
@@ -6,4 +6,4 @@ THUMBNAIL_OUTPUT_PATH=local/thumb.webp
 
 ffmpeg -y -i $VIDEO_INPUT_PATH -vframes 1 -ss $2 -filter:v scale='480:-1' -quality 70 $THUMBNAIL_OUTPUT_PATH
 
-aws s3 cp --endpoint-url "nyc3.digitaloceanspaces.com" $THUMBNAIL_OUTPUT_PATH s3://bken-sandbox-dev/thumb.jpg
+aws s3 cp --endpoint-url "https://nyc3.digitaloceanspaces.com" $THUMBNAIL_OUTPUT_PATH s3://bken-sandbox-dev/thumb.jpg
