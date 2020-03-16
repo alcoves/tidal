ce394f1779dd1707ce3f476b801195e980029cda
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 01:09:51 -0400

    Use pipe for thumbnailer

--- scripts/thumbnailer.sh
+++ scripts/thumbnailer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-ffmpeg -i $1 -vframes 1 -ss $2 -filter:v scale='720:-1' | aws s3 cp - s3://bken-tidal-dev/test/thumb.jpg
+ffmpeg -i $1 -vframes 1 -ss $2 -filter:v scale='720:-1' -f mp4 -movflags frag_keyframe+faststart pipe:1 | aws s3 cp - s3://bken-tidal-dev/test/thumb.jpg
