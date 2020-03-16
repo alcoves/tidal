d104cd6fa75514f61b519185fc343441274d5245
Brendan Kennedy <brenwken@gmail.com>
2020-03-16 05:18:13 -0400

    Transcode

--- scripts/transcode.sh
+++ scripts/transcode.sh
@@ -4,8 +4,8 @@ set -e
 VIDEO_INPUT_PATH=local/video
 VIDEO_OUTPUT_PATH=local/out.mp4
 
-echo "Video Path $VIDEO_PATH"
+echo "Video Path $VIDEO_INPUT_PATH"
 
-ffmpeg -i $VIDEO_PATH -c:v libx264 -crf 22 $VIDEO_OUTPUT_PATH
+ffmpeg -i $VIDEO_INPUT_PATH -c:v libx264 -crf 22 $VIDEO_OUTPUT_PATH
 
 aws s3 cp $VIDEO_OUTPUT_PATH s3://bken-sandbox-dev/720p-converted.mp4
