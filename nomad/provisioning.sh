4bf26afedf6303e9243fa178b13c3fac039be64d
Brendan Kennedy <brenwken@gmail.com>
2020-03-18 11:43:43 -0400

    Disable client

--- nomad/provisioning.sh
+++ nomad/provisioning.sh
@@ -14,13 +14,9 @@ mv nomad /usr/local/bin
 # Clone tidal
 git clone https://github.com/bken-io/tidal.git
 
-# Setup nomad to run via systemctl
-sudo mkdir -p /etc/nomad
-sudo cp tidal/nomad/client.hcl /etc/nomad/client.hcl
-sudo cp tidal/nomad/server.hcl /etc/nomad/server.hcl
-
-sudo cp tidal/nomad/client.service /etc/systemd/system/nomad-client.service
-sudo cp tidal/nomad/server.service /etc/systemd/system/nomad-server.service
+# Create service definitions
+sudo cp /root/tidal/nomad/client.service /etc/systemd/system/nomad-client.service
+sudo cp /root/tidal/nomad/server.service /etc/systemd/system/nomad-server.service
 
 sudo systemctl enable nomad-server.service
 sudo systemctl start nomad-server.service
