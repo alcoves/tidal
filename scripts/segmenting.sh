1dea04bcbb483dda399b5d6746cc39f391dbcc9d
Brendan Kennedy <brenwken@gmail.com>
2020-03-17 22:08:43 -0400

    Docker segmenting

--- scripts/segmenting.sh
+++ scripts/segmenting.sh
@@ -4,30 +4,36 @@ set -e
 echo "Setting envs"
 BUCKET=$1
 VIDEO_ID=$2
-AWS_ACCESS_KEY_ID=$3
-AWS_ACCESS_KEY_SECRET=$4
+FILENAME=$3
+AWS_ACCESS_KEY_ID=$4
+AWS_ACCESS_KEY_SECRET=$5
 
-IN_PATH="local/file"
-SEGMENTS_DIR="local/segments"
-AUDIO_PATH="local/source.wav"
+TMP_DIR=$(mktemp -d)
+SEGMENTS_DIR="$TMP_DIR/segments"
+AUDIO_PATH="$TMP_DIR/source.wav"
+SOURCE_VIDEO="$TMP_DIR/$FILENAME"
+DO_ENDPOINT="nyc3.digitaloceanspaces.com"
+
+echo "Downloading source clip"
+aws s3 cp s3://$BUCKET/uploads/$VIDEO_ID/$FILENAME $SOURCE_VIDEO --endpoint $DO_ENDPOINT
 
 echo "Create data directories"
 mkdir -p $SEGMENTS_DIR
 
 echo "Exporting audio"
-ffmpeg -i $IN_PATH -threads 1 $AUDIO_PATH
+ffmpeg -i $SOURCE_VIDEO -threads 1 $AUDIO_PATH
 
 echo "Uploading audio"
 s3cmd put -c local/.s3cfg $AUDIO_PATH s3://$BUCKET/audio/$VIDEO_ID/source.wav
 
 echo "Segmenting video"
-ffmpeg -i $IN_PATH -y -map 0 -threads 1 -c copy -f segment -segment_time 10 -an $SEGMENTS_DIR/output_%04d.mkv
+ffmpeg -i $SOURCE_VIDEO -y -map 0 -threads 1 -c copy -f segment -segment_time 10 -an $SEGMENTS_DIR/output_%04d.mkv
 
 echo "Segmentation complete"
 ls $SEGMENTS_DIR/
 
 echo "Getting video metadata"
-METADATA=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 $IN_PATH)
+METADATA=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 $SOURCE_VIDEO)
 ARR=(${METADATA//,/ })
 WIDTH=${ARR[0]}
 HEIGHT=${ARR[1]}
