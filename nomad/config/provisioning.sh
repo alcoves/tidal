669f8a1123806184680c0430c9c3ea7d364fafc7
Brendan Kennedy <brenwken@gmail.com>
2020-03-20 16:21:44 -0400

    Update terraform config

--- nomad/config/provisioning.sh
+++ nomad/config/provisioning.sh
@@ -17,8 +17,8 @@ git clone https://github.com/bken-io/tidal.git
 cd tidal && git checkout dev && cd ~
 
 # Create service definitions
-sudo cp /root/tidal/nomad/config/client.service /etc/systemd/system/nomad-client.service
-sudo cp /root/tidal/nomad/config/server.service /etc/systemd/system/nomad-server.service
+sudo cp ~/tidal/nomad/config/client.service /etc/systemd/system/nomad-client.service
+sudo cp ~/tidal/nomad/config/server.service /etc/systemd/system/nomad-server.service
 
 sudo systemctl enable nomad-server.service
 sudo systemctl start nomad-server.service
