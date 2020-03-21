844215d2f6e7436623d6a944d8282bcedc9f159d
Brendan Kennedy <brenwken@gmail.com>
2020-03-21 00:39:10 -0400

    Transcoding fix

--- stack/modules/transcoding/src/transcoding.sh
+++ stack/modules/transcoding/src/transcoding.sh
@@ -13,7 +13,8 @@ function handler () {
   echo $FFMPEG_COMMAND
 
   echo "transcoding started"
-  # aws s3 cp $IN_PATHIN_PATH
-  aws s3 cp $IN_PATH - | /opt/ffmpeg/ffmpeg -i - $FFMPEG_COMMAND - | aws s3 cp - $OUT_PATH
+  aws s3 cp $IN_PATH /tmp/in
+  aws s3 cp /tmp/in - | /opt/ffmpeg/ffmpeg -i /tmp/in $FFMPEG_COMMAND /tmp/out.mkv
+  aws s3 cp /tmp/out.mkv $OUT_PATH
   echo "transcoding completed"
 }
