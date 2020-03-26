1c5b4267002b542c8c2b41bd78505c4d34acc5a0
Brendan Kennedy <brenwken@gmail.com>
2020-03-25 22:21:08 -0400

    Enable PITR on tidal db

--- stack/modules/core/db.tf
+++ stack/modules/core/db.tf
@@ -1,8 +1,10 @@
 resource "aws_dynamodb_table" "tidal_db" {
-  hash_key       = "id"
-  range_key      = "preset"
-  name           = "tidal-${var.env}"
-  billing_mode   = "PAY_PER_REQUEST"
+  stream_enabled         = true
+  point_in_time_recovery = true
+  hash_key               = "id"
+  range_key              = "preset"
+  name                   = "tidal-${var.env}"
+  billing_mode           = "PAY_PER_REQUEST"
 
   attribute {
     name = "id"
