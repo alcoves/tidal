c5d24ff05ceb73791c21ccb10505e4d8e59de7c4
Brendan Kennedy <brenwken@gmail.com>
2020-03-31 20:43:06 -0400

    Update uploading job

--- docs/examples/uploading.sh
+++ docs/examples/uploading.sh
@@ -1,4 +1,11 @@
 nomad job dispatch -detach \
-  -meta "env=dev" \
   -meta "github_access_token=$GITHUB_ACCESS_TOKEN" \
-  uploads_v2
+  -meta "wasabi_access_key_id=$WASABI_ACCESS_KEY_ID" \
+  -meta "wasabi_secret_access_key=$WASABI_SECRET_ACCESS_KEY" \
+  uploads_dev
+
+nomad job dispatch -detach \
+  -meta "github_access_token=$GITHUB_ACCESS_TOKEN" \
+  -meta "wasabi_access_key_id=$WASABI_ACCESS_KEY_ID" \
+  -meta "wasabi_secret_access_key=$WASABI_SECRET_ACCESS_KEY" \
+  uploads_prod
