8f14bf4dbd3ff0a35de46926bcd4a3640c72dac5
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 19:07:28 -0400

    Add prod terraform config

--- stack/prod/main.tf
+++ stack/prod/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region = "us-east-1"
     bucket = "bken-tf-state"
-    key    = "services/tidal/dev/tidal.tfstate"
+    key    = "services/tidal/prod/tidal.tfstate"
   }
 }
 
@@ -19,13 +19,30 @@ module "core" {
   source    = "../../modules/core"
 }
 
+module "transcoding" {
+  env    = var.env
+  source = "../../modules/transcoding"
+}
+
 module "uploading" {
   env               = var.env
   source            = "../../modules/uploading"
   uploads_queue_arn = module.core.uploads_queue_arn
 }
 
-module "transcoding" {
-  env    = var.env
-  source = "../../modules/transcoding"
+module "segmenting" {
+  env                   = var.env
+  registry_secrets_arn  = "github_registry_login"
+  source                = "../../modules/segmenting"
+  table_name            = module.core.tidal_table_name
+  transcoding_queue_url = "https://sqs.us-east-1.amazonaws.com/594206825329/${module.transcoding.transcoding_queue_name}" 
+  app_image             = "docker.pkg.github.com/bken-io/tidal/tidal:latest"
+}
+
+module "concatinating" {
+  env                   = var.env
+  registry_secrets_arn  = "github_registry_login"
+  source                = "../../modules/concatinating"
+  table_name            = module.core.tidal_table_name
+  app_image             = "docker.pkg.github.com/bken-io/tidal/tidal:latest"
 }
