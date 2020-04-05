b63462a09717a71059614bb8b543563ec0dc275a
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 18:04:50 -0400

    Remove nomad jobs

--- docs/examples/segmenting.sh
+++
@@ -1,6 +0,0 @@
-nomad job dispatch -detach \
-  -meta "video_id=test" \
-  -meta "filename=source.mp4" \
-  -meta "bucket=tidal-bken-dev" \
-  -meta "transcode_queue_url=https://sqs.us-east-1.amazonaws.com/594206825329/tidal-transcoding-dev" \
-  segmenting_dev
