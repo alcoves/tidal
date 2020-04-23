0eb3643d9109f477a0eee09a04ce34f1367ac808
Brendan Kennedy <brenwken@gmail.com>
2020-04-22 20:30:42 -0400

    Rust formatting

--- src/concat/mod.rs
+++ src/concat/mod.rs
@@ -3,67 +3,66 @@ use std::process::Command;
 use std::{fs, io};
 
 fn create_manifest() -> String {
-    let path = "./tmp/concat-manifest.txt";
-    let tmp_dir = String::from("./tmp");
-    let seg_path = format!("{}/segments", tmp_dir);
-    let mut segment_list = vec![];
+  let path = "./tmp/concat-manifest.txt";
+  let tmp_dir = String::from("./tmp");
+  let seg_path = format!("{}/segments", tmp_dir);
+  let mut segment_list = vec![];
 
-    let mut paths: Vec<_> = fs::read_dir(seg_path).unwrap()
-    .map(|r| r.unwrap())
-    .collect();
+  let mut paths: Vec<_> =
+    fs::read_dir(seg_path).unwrap().map(|r| r.unwrap()).collect();
 
-    paths.sort_by_key(|dir| dir.path());
+  paths.sort_by_key(|dir| dir.path());
 
-    for path in paths {
-        let seg_path = path.path().display().to_string().replace(&tmp_dir, ".");
-        let ffmpeg_import = format!("file '{}'", seg_path);
-        println!("{}", ffmpeg_import);
-        segment_list.push(ffmpeg_import);
-    }
+  for path in paths {
+    let seg_path = path.path().display().to_string().replace(&tmp_dir, ".");
+    let ffmpeg_import = format!("file '{}'", seg_path);
+    println!("{}", ffmpeg_import);
+    segment_list.push(ffmpeg_import);
+  }
 
-    let joined = segment_list.join("\n");
-    fs::write(path, joined).expect("Unable to write file");
+  let joined = segment_list.join("\n");
+  fs::write(path, joined).expect("Unable to write file");
 
-    return path.to_owned();
+  return path.to_owned();
 }
 
 pub fn run(args: clap::ArgMatches) {
-    println!("Invoking concatination pipeline");
+  println!("Invoking concatination pipeline");
 
-    let tmp_dir = "./tmp";
-    let segment_dir = format!("{}/transcoded", tmp_dir);
+  let tmp_dir = "./tmp";
+  let segment_dir = format!("{}/transcoded", tmp_dir);
 
-    let source_audio_path = "./tmp/test.wav";
-    let out_audio_video_path = "./tmp/converted.mp4";
-    let out_video_path = "./tmp/converted-no-audio.mp4";
-    let manifest_path = create_manifest();
+  let source_audio_path = "./tmp/test.wav";
+  let out_audio_video_path = "./tmp/converted.mp4";
+  let out_video_path = "./tmp/converted-no-audio.mp4";
+  let manifest_path = create_manifest();
 
-    println!("Concatinating segments");
-    let _ffmpeg_seg = Command::new("ffmpeg")
-        .arg("-y")
-        .arg("-f")
-        .arg("concat")
-        .arg("-safe")
-        .arg("0")
-        .arg("-i")
-        .arg(manifest_path)
-        .arg("-c")
-        .arg("copy")
-        .arg(out_video_path)
-        .output();
+  println!("Concatinating segments");
+  let _ffmpeg_seg = Command::new("ffmpeg")
+    .arg("-y")
+    .arg("-f")
+    .arg("concat")
+    .arg("-safe")
+    .arg("0")
+    .arg("-i")
+    .arg(manifest_path)
+    .arg("-c")
+    .arg("copy")
+    .arg(out_video_path)
+    .output();
 
-    println!("Adding source audio");
-    let _ffmpeg_seg = Command::new("ffmpeg")
-        .arg("-y")
-        .arg("-i")
-        .arg(out_video_path)
-        .arg("-i")
-        .arg(source_audio_path)
-        .arg("-c:v")
-        .arg("copy")
-        .arg(out_audio_video_path)
-        .output();
+  println!("Adding source audio");
+  let _ffmpeg_seg = Command::new("ffmpeg")
+    .arg("-y")
+    .arg("-i")
+    .arg(out_video_path)
+    .arg("-i")
+    .arg(source_audio_path)
+    .arg("-c:v")
+    .arg("copy")
+    .arg(out_audio_video_path)
+    .output();
 
-    // upload to wasabi cloud storage
-    // update database with done status
+  // upload to wasabi cloud storage
+  // update database with done status
 }
--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,46 @@ mod concat;
 mod segment;
 
 fn main() {
-    dotenv().ok();
+  dotenv().ok();
 
-    // for (key, value) in env::vars() {
-    //     println!("{}: {}", key, value);
-    // }
+  // for (key, value) in env::vars() {
+  //     println!("{}: {}", key, value);
+  // }
 
-    let matches = App::new("Tidal Distributed Video Transcoder")
-        .version("0.1.0")
-        .author("Brendan Kennedy <brenwken@gmail.com>")
-        .about("fast video transcoder written in rust")
-        .arg(
-            Arg::with_name("mode")
-                .short('m')
-                .long("mode")
-                .takes_value(true)
-                .help("One of segment, transcode, or concat"),
-        )
-        .arg(
-            Arg::with_name("in")
-                .short('i')
-                .long("in")
-                .takes_value(true)
-                .help("Path to source"),
-        )
-        .arg(
-            Arg::with_name("out")
-                .short('o')
-                .long("out")
-                .takes_value(true)
-                .help("Path to destination"),
-        )
-        .get_matches();
+  let matches = App::new("Tidal Distributed Video Transcoder")
+    .version("0.1.0")
+    .author("Brendan Kennedy <brenwken@gmail.com>")
+    .about("fast video transcoder written in rust")
+    .arg(
+      Arg::with_name("mode")
+        .short('m')
+        .long("mode")
+        .takes_value(true)
+        .help("One of segment, transcode, or concat"),
+    )
+    .arg(
+      Arg::with_name("in")
+        .short('i')
+        .long("in")
+        .takes_value(true)
+        .help("Path to source"),
+    )
+    .arg(
+      Arg::with_name("out")
+        .short('o')
+        .long("out")
+        .takes_value(true)
+        .help("Path to destination"),
+    )
+    .get_matches();
 
-    println!("{:?}", matches);
+  println!("{:?}", matches);
 
-    let mode = matches.value_of("mode");
+  let mode = matches.value_of("mode");
 
-    match mode {
-        Some("segment") => segment::run(matches),
-        Some("concat") => concat::run(matches),
-        _ => println!("invalid mode: {}", mode.unwrap()),
-    }
+  match mode {
+    Some("segment") => segment::run(matches),
+    Some("concat") => concat::run(matches),
+    _ => println!("invalid mode: {}", mode.unwrap()),
+  }
 }
