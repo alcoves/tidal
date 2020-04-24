197146e0d9be2befa999a24ccb24dc7d970698e7
Brendan Kennedy <brenwken@gmail.com>
2020-04-23 22:10:51 -0400

    Cleanup arguments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate dotenv;
 
 use clap::{App, Arg};
 use dotenv::dotenv;
-// use std::env;
 
 mod concat;
 mod segment;
@@ -11,43 +10,29 @@ mod segment;
 fn main() {
   dotenv().ok();
 
-  // for (key, value) in env::vars() {
-  //     println!("{}: {}", key, value);
-  // }
-
   let matches = App::new("Tidal Distributed Video Transcoder")
     .version("0.1.0")
     .author("Brendan Kennedy <brenwken@gmail.com>")
     .about("fast video transcoder written in rust")
+    .arg(Arg::with_name("mode").index(1).required(true))
+    .arg(Arg::with_name("in").index(2).required(true))
+    .arg(Arg::with_name("out").index(3).required(true))
     .arg(
-      Arg::with_name("mode")
-        .short('m')
-        .long("mode")
+      Arg::with_name("transcode_queue_url")
         .takes_value(true)
-        .help("One of segment, transcode, or concat"),
-    )
-    .arg(
-      Arg::with_name("in")
-        .short('i')
-        .long("in")
-        .takes_value(true)
-        .help("Path to source"),
-    )
-    .arg(
-      Arg::with_name("out")
-        .short('o')
-        .long("out")
-        .takes_value(true)
-        .help("Path to destination"),
+        .long("transcodeQueueUrl"),
     )
     .get_matches();
 
-  println!("{:?}", matches);
-
   let mode = matches.value_of("mode");
+  let arg_in = &matches.value_of("in").unwrap();
+  let arg_out = &matches.value_of("out").unwrap();
 
   match mode {
-    Some("segment") => segment::run(matches),
+    Some("segment") => {
+      println!("Segmentation Mode");
+      segment::run(arg_in, arg_out)
+    }
     Some("concat") => concat::run(matches),
     _ => println!("invalid mode: {}", mode.unwrap()),
   }
