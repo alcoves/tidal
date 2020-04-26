ef927e29223adcfed5f98ab37a0b73b892dfd197
Brendan Kennedy <brenwken@gmail.com>
2020-04-25 20:25:30 -0400

    Revert back to sdk cli

--- src/segment/download.rs
+++ src/segment/download.rs
@@ -1,22 +1,19 @@
-use rusoto_s3::{GetObjectRequest, S3Client, S3};
-use std::default::Default;
-use std::fs::File;
-use std::io::Write;
+use std::process::Command;
 
-pub async fn get_object(client: S3Client, bucket: &str, key: &str) {
-  let s3file = client
-    .get_object(GetObjectRequest {
-      bucket: bucket.to_owned(),
-      key: key.to_owned(),
-      ..Default::default()
-    })
-    .await
-    .expect("failed");
+pub async fn get_object(bucket: &str, key: &str, dest_path: &str) {
+  let source_path = format!("s3://{}/{}", bucket, key);
 
-  let mut stream = s3file.body.unwrap().into_async_read();
-  // let mut body = Vec::new();
-  // stream.read_to_end(&mut body).await.unwrap();
+  let output = Command::new("aws")
+    .arg("s3")
+    .arg("cp")
+    .arg(source_path)
+    .arg(dest_path)
+    .output()
+    .unwrap();
 
-  let mut file = File::create("./tmp/test.mp4");
-  // file.write_all(body).expect("failed to write body");
+  println!("status: {}", output.status);
+  println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+  println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+  assert!(output.status.success());
 }
