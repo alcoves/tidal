bb9b28e26706aaab13588942e76f6fc980482c90
Brendan Kennedy <brenwken@gmail.com>
2020-04-27 16:18:31 -0400

    Linting

--- src/main.rs
+++ src/main.rs
@@ -19,29 +19,21 @@ fn main() {
       Arg::with_name("video_id")
         .required(true)
         .takes_value(true)
-        .long("video_id")
+        .long("video_id"),
     )
     .arg(
       Arg::with_name("bucket_name")
         .short('b')
         .required(true)
         .takes_value(true)
-        .long("bucket_name")
+        .long("bucket_name"),
     )
-    .arg(
-      Arg::with_name("filename")
-        .takes_value(true)
-        .long("filename")
-    )
-    .arg(
-      Arg::with_name("preset")
-        .takes_value(true)
-        .long("preset")
-    )
+    .arg(Arg::with_name("filename").takes_value(true).long("filename"))
+    .arg(Arg::with_name("preset").takes_value(true).long("preset"))
     .arg(
       Arg::with_name("transcode_queue_url")
         .takes_value(true)
-        .long("transcode_queue_url")
+        .long("transcode_queue_url"),
     )
     .get_matches();
 
