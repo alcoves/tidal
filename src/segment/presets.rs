1991e7aa004e641080a7c7b253726c22a4a6aa29
Brendan Kennedy <brenwken@gmail.com>
2020-04-26 22:17:46 -0400

    Fix bf2 preset value

--- src/segment/presets.rs
+++ src/segment/presets.rs
@@ -15,31 +15,31 @@ pub fn presets(width: u16) -> Vec<Preset> {
   let libx264_480p = Preset {
       extension: "mkv".to_string(),
       name: "libx264_480p".to_string(),
-      cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=854-2 -crf 28 -coder 1 -pix_fmt yuv420p -bf2".to_string()
+      cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=854-2 -crf 28 -coder 1 -pix_fmt yuv420p -bf 2".to_string()
   };
 
   let libx264_720p = Preset {
     extension: "mkv".to_string(),
     name: "libx264_720p".to_string(),
-    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=1280:-2 -crf 25 -coder 1 -pix_fmt yuv420p -bf2".to_string()
+    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=1280:-2 -crf 25 -coder 1 -pix_fmt yuv420p -bf 2".to_string()
   };
 
   let libx264_1080p = Preset {
     extension: "mkv".to_string(),
     name: "libx264_1080p".to_string(),
-    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=1920:-2 -crf 24 -coder 1 -pix_fmt yuv420p -bf2".to_string()
+    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=1920:-2 -crf 24 -coder 1 -pix_fmt yuv420p -bf 2".to_string()
   };
 
   let libx264_1440p = Preset {
     extension: "mkv".to_string(),
     name: "libx264_1440p".to_string(),
-    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=2560:-2 -crf 23 -coder 1 -pix_fmt yuv420p -bf2".to_string()
+    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=2560:-2 -crf 23 -coder 1 -pix_fmt yuv420p -bf 2".to_string()
   };
 
   let libx264_2160p = Preset {
     extension: "mkv".to_string(),
     name: "libx264_3840p".to_string(),
-    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=3840:-2 -crf 22 -coder 1 -pix_fmt yuv420p -bf2".to_string()
+    cmd: "-c:v libx264 -preset medium -profile:v high -vf scale=3840:-2 -crf 22 -coder 1 -pix_fmt yuv420p -bf 2".to_string()
   };
 
   let mut presets: Vec<Preset> = vec![];
