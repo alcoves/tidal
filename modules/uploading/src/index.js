04705b7c5e639cfcb5d4b3bbb69bb1905d3a3c96
Brendan Kennedy <brenwken@gmail.com>
2020-04-30 14:59:04 -0400

    Convert audio to opus

--- modules/uploading/src/index.js
+++ modules/uploading/src/index.js
@@ -1,5 +1,5 @@
-const AWS = require("aws-sdk");
-const lambda = new AWS.Lambda({ region: "us-east-1" });
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
 
 module.exports.handler = async (event) => {
   console.log(JSON.stringify(event, null, 2));
@@ -7,8 +7,8 @@ module.exports.handler = async (event) => {
     const { Records } = JSON.parse(body);
     for (const { s3 } of Records) {
       const bucket = s3.bucket.name;
-      const videoId = s3.object.key.split("/")[1];
-      const filename = s3.object.key.split("/")[2];
+      const videoId = s3.object.key.split('/')[1];
+      const filename = s3.object.key.split('/')[2];
 
       const segRes = await lambda
         .invokeAsync({
@@ -17,19 +17,20 @@ module.exports.handler = async (event) => {
         })
         .promise();
 
-      console.log("segRes", segRes);
+      console.log('segRes', segRes);
 
       const audioRes = await lambda
         .invokeAsync({
           FunctionName: process.env.AUDIO_EXTRACTOR_FN_NAME,
           InvokeArgs: JSON.stringify({
+            ffmpeg_cmd: `-vn -c:a libopus -f opus`,
+            out_path: `s3://${bucket}/audio/${videoId}/source.ogg`,
             in_path: `s3://${bucket}/uploads/${videoId}/${filename}`,
-            out_path: `s3://${bucket}/audio/${videoId}/source.wav`,
           }),
         })
         .promise();
 
-      console.log("audioRes", audioRes);
+      console.log('audioRes', audioRes);
     }
   }
 };
