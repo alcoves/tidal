d446836b685bfa8fa4a3ac0f11c10ba7c5da7284
Brendan Kennedy <brenwken@gmail.com>
2020-04-30 20:00:04 -0400

    Default to using vp9 presets only

--- modules/segmenting/src/lib/getPresets.js
+++ modules/segmenting/src/lib/getPresets.js
@@ -5,23 +5,18 @@ module.exports = (width) => {
   const selectedPresets = [];
 
   selectedPresets.push({
-    presetName: 'libx264-480p',
-    ffmpegCmdStr: presets['libx264-480p'].cmd.join(' '),
+    presetName: 'libvpx_vp9-480p',
+    ffmpegCmdStr: presets['libvpx_vp9-480p'].cmd.join(' '),
   });
 
   if (width >= 1280) {
     selectedPresets.push({
-      presetName: 'libx264-720p',
-      ffmpegCmdStr: presets['libx264-720p'].cmd.join(' '),
+      presetName: 'libvpx_vp9-720p',
+      ffmpegCmdStr: presets['libvpx_vp9-720p'].cmd.join(' '),
     });
   }
 
   if (width >= 1920) {
-    selectedPresets.push({
-      presetName: 'libx264-1080p',
-      ffmpegCmdStr: presets['libx264-1080p'].cmd.join(' '),
-    });
-
     selectedPresets.push({
       presetName: 'libvpx_vp9-1080p',
       ffmpegCmdStr: presets['libvpx_vp9-1080p'].cmd.join(' '),
@@ -30,15 +25,15 @@ module.exports = (width) => {
 
   if (width >= 2560) {
     selectedPresets.push({
-      presetName: 'libx264-1440p',
-      ffmpegCmdStr: presets['libx264-1440p'].cmd.join(' '),
+      presetName: 'libvpx_vp9-1440p',
+      ffmpegCmdStr: presets['libvpx_vp9-1440p'].cmd.join(' '),
     });
   }
 
   if (width >= 3840) {
     selectedPresets.push({
-      presetName: 'libx264-2160p',
-      ffmpegCmdStr: presets['libx264-2160p'].cmd.join(' '),
+      presetName: 'libvpx_vp9-2160p',
+      ffmpegCmdStr: presets['libvpx_vp9-2160p'].cmd.join(' '),
     });
   }
 
