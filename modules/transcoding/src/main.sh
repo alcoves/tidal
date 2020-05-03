47e416db515147f37ab618b1f2337253af45ecc4
Brendan Kennedy <brenwken@gmail.com>
2020-05-03 12:22:50 -0400

    Modify transcoder to use pipes instead of local disk

--- modules/transcoding/src/main.sh
+++ modules/transcoding/src/main.sh
@@ -1,9 +1,6 @@
 function handler () {
   set -e
 
-  echo "Cleaning up lambda"
-  rm -rf /tmp/tmp*
-
   SQS_BODY=$(echo $1 | jq -r '.Records[0].body')
   echo "SQS_BODY: $SQS_BODY"
 
@@ -11,23 +8,13 @@ function handler () {
   OUT_PATH="$(echo $SQS_BODY | jq -r '.out_path')"
   FFMPEG_COMMAND=$(echo $SQS_BODY | jq -r '.ffmpeg_cmd')
 
-  LVIP_DIR=$(mktemp -d)
-  LVOP_DIR=$(mktemp -d)
-
-  LVIP="$LVIP_DIR/${IN_PATH##*/}"
-  LVOP="$LVOP_DIR/${OUT_PATH##*/}"
-
   echo $IN_PATH
   echo $OUT_PATH
   echo $FFMPEG_COMMAND
 
   echo "transcoding started"
-  aws s3 cp $IN_PATH $LVIP
-  /opt/ffmpeg/ffmpeg -y -i $LVIP $FFMPEG_COMMAND $LVOP
-  aws s3 cp $LVOP $OUT_PATH
-
-  echo "Cleaning up lambda"
-  rm -rf /tmp/tmp*
+  SIGNED_IN_URL=$(aws s3 presign $IN_PATH)
+  /opt/ffmpeg/ffmpeg -i "$SIGNED_IN_URL" $FFMPEG_COMMAND | aws s3 cp - $OUT_PATH
   
   echo "transcoding completed"
 }
