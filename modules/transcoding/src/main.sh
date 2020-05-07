72cd4f3c454558b07d9c6641d97c2e2e11246c16
Brendan Kennedy <brenwken@gmail.com>
2020-05-06 21:51:17 -0400

    Write segments to disk locally

--- modules/transcoding/src/main.sh
+++ modules/transcoding/src/main.sh
@@ -1,5 +1,6 @@
 function handler () {
   set -e
+  rm -f /tmp/out.mkv
 
   SQS_BODY=$(echo $1 | jq -r '.Records[0].body')
   echo "SQS_BODY: $SQS_BODY"
@@ -14,7 +15,9 @@ function handler () {
 
   echo "transcoding started"
   SIGNED_IN_URL=$(aws s3 presign $IN_PATH)
-  /opt/ffmpeg/ffmpeg -i "$SIGNED_IN_URL" $FFMPEG_COMMAND | aws s3 cp - $OUT_PATH
+  /opt/ffmpeg/ffmpeg -i "$SIGNED_IN_URL" $FFMPEG_COMMAND /tmp/out.mkv
+  aws s3 cp /tmp/out.mkv $OUT_PATH
   
+  rm -f /tmp/out.mkv
   echo "transcoding completed"
 }
