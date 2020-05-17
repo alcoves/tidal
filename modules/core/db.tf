533f483aa5e2a5d9b481b06d66dae561dca9c006
Brendan Kennedy <brenwken@gmail.com>
2020-05-16 23:10:23 -0400

    Add stream mode to database

--- modules/core/db.tf
+++ modules/core/db.tf
@@ -1,8 +1,9 @@
 resource "aws_dynamodb_table" "tidal_db" {
+  stream_enabled = true
   hash_key       = "id"
   range_key      = "preset"
-  name           = "tidal-${var.env}"
   billing_mode   = "PAY_PER_REQUEST"
+  name           = "tidal-${var.env}"
 
   attribute {
     name = "id"
