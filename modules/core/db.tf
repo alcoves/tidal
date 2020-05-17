b678665a7971f50717793c0c7345bfdb89d1cff2
Brendan Kennedy <brenwken@gmail.com>
2020-05-17 00:38:34 -0400

    Add stream_view_type

--- modules/core/db.tf
+++ modules/core/db.tf
@@ -1,9 +1,10 @@
 resource "aws_dynamodb_table" "tidal_db" {
-  stream_enabled = true
-  hash_key       = "id"
-  range_key      = "preset"
-  billing_mode   = "PAY_PER_REQUEST"
-  name           = "tidal-${var.env}"
+  stream_enabled   = true
+  hash_key         = "id"
+  range_key        = "preset"
+  billing_mode     = "PAY_PER_REQUEST"
+  name             = "tidal-${var.env}"
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
