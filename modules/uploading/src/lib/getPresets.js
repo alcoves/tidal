434b1c2c6d7c4a50ecd8e133f1419d4564c48ebb
Brendan Kennedy <brenwken@gmail.com>
2020-05-19 00:44:19 -0400

    Prepare libx264 presets

--- modules/uploading/src/lib/getPresets.js
+++ modules/uploading/src/lib/getPresets.js
@@ -8,11 +8,21 @@ module.exports = (width) => {
     ffmpegCmdStr: presets['libvpx_vp9-480p'].cmd.join(' '),
   });
 
+  // selectedPresets.push({
+  //   presetName: 'libx264-480p',
+  //   ffmpegCmdStr: presets['libx264-480p'].cmd.join(' '),
+  // });
+
   if (width >= 1280) {
     selectedPresets.push({
       presetName: 'libvpx_vp9-720p',
       ffmpegCmdStr: presets['libvpx_vp9-720p'].cmd.join(' '),
     });
+
+    // selectedPresets.push({
+    //   presetName: 'libx264-720p',
+    //   ffmpegCmdStr: presets['libx264-720p'].cmd.join(' '),
+    // });
   }
 
   if (width >= 1920) {
@@ -20,6 +30,11 @@ module.exports = (width) => {
       presetName: 'libvpx_vp9-1080p',
       ffmpegCmdStr: presets['libvpx_vp9-1080p'].cmd.join(' '),
     });
+
+    // selectedPresets.push({
+    //   presetName: 'libx264-1080p',
+    //   ffmpegCmdStr: presets['libx264-1080p'].cmd.join(' '),
+    // });
   }
 
   if (width >= 2560) {
@@ -27,6 +42,11 @@ module.exports = (width) => {
       presetName: 'libvpx_vp9-1440p',
       ffmpegCmdStr: presets['libvpx_vp9-1440p'].cmd.join(' '),
     });
+
+    // selectedPresets.push({
+    //   presetName: 'libx264-1440p',
+    //   ffmpegCmdStr: presets['libx264-1440p'].cmd.join(' '),
+    // });
   }
 
   if (width >= 3840) {
@@ -34,6 +54,11 @@ module.exports = (width) => {
       presetName: 'libvpx_vp9-2160p',
       ffmpegCmdStr: presets['libvpx_vp9-2160p'].cmd.join(' '),
     });
+
+    // selectedPresets.push({
+    //   presetName: 'libx264-2160p',
+    //   ffmpegCmdStr: presets['libx264-2160p'].cmd.join(' '),
+    // });
   }
 
   return selectedPresets;
