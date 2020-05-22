2bc355a0c4218d402eded0e9bb13356cd568cbed
Brendan Kennedy <brenwken@gmail.com>
2020-05-21 20:07:59 -0400

    Revert back to signed audio urls

--- modules/audio_extractor/src/main.sh
+++ modules/audio_extractor/src/main.sh
@@ -5,8 +5,10 @@ function handler () {
   IN_PATH="$(echo $BODY | jq -r '.in_path')"
   OUT_PATH="$(echo $BODY | jq -r '.out_path')"
   FFMPEG_CMD="$(echo $BODY | jq -r '.ffmpeg_cmd')"
+  
+  SIGNED_AUDIO_URL=$(aws s3 presign $IN_PATH)
 
   echo "in: $IN_PATH | out: $OUT_PATH"
-  aws s3 cp $IN_PATH - | /opt/ffmpeg/ffmpeg -i - $FFMPEG_CMD - | aws s3 cp - $OUT_PATH
+  /opt/ffmpeg/ffmpeg -i "$SIGNED_AUDIO_URL" $FFMPEG_CMD - | aws s3 cp - $OUT_PATH
   echo "audio extraction completed"
 }
