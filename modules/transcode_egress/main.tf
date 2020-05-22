9e6b6cf5f6c2c57debcb3c5a515bfad820cc1952
Brendan Kennedy <brenwken@gmail.com>
2020-05-22 00:27:29 -0400

    Fix CDN egress

--- modules/transcode_egress/main.tf
+++ modules/transcode_egress/main.tf
@@ -39,19 +39,19 @@ resource "aws_lambda_function" "transcode_egress" {
     "arn:aws:lambda:us-east-1:744348701589:layer:bash:8",
   ]
 
-  vpc_config {
-    security_group_ids = [
-      "sg-665de11a",
-    ]
-    subnet_ids = [
-      "subnet-00bcc265",
-      "subnet-11635158",
-      "subnet-2c4a0701",
-      "subnet-9446c4a8",
-      "subnet-c7275c9c",
-      "subnet-fd3a56f1",
-    ]
-  }
+  // vpc_config {
+  //   security_group_ids = [
+  //     "sg-665de11a",
+  //   ]
+  //   subnet_ids = [
+  //     "subnet-00bcc265",
+  //     "subnet-11635158",
+  //     "subnet-2c4a0701",
+  //     "subnet-9446c4a8",
+  //     "subnet-c7275c9c",
+  //     "subnet-fd3a56f1",
+  //   ]
+  // }
 }
 
 resource "aws_cloudwatch_log_group" "transcode_egress" {
