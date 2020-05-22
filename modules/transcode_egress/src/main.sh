1d33269dcb2278a0778c59d7079d75a65ef5ff0d
Brendan Kennedy <brenwken@gmail.com>
2020-05-22 00:27:29 -0400

    Fix CDN egress

--- modules/transcode_egress/src/main.sh
+++ modules/transcode_egress/src/main.sh
@@ -2,13 +2,21 @@ function handler () {
   set -e
   EVENT_DATA=$1
   
-  EVENT_NAME=$(echo $EVENT_DATA | jq -r '.Records[0].eventName')
   KEY=$(echo $EVENT_DATA | jq -r '.Records[0].s3.object.key')
+  EVENT_NAME=$(echo $EVENT_DATA | jq -r '.Records[0].eventName')
   BUCKET=$(echo $EVENT_DATA | jq -r '.Records[0].s3.bucket.name')
+
+  VIDEO_ID=$(echo $KEY | cut -d'/' -f2)
+  FILENAME=$(echo $KEY | cut -d'/' -f3)
     
-  echo $KEY
-  echo $BUCKET
-  echo $EVENT_NAME
+  echo "KEY: $KEY"
+  echo "BUCKET: $BUCKET"
+  echo "VIDEO_ID: $VIDEO_ID"
+  echo "FILENAME: $FILENAME"
+  echo "EVENT_NAME: $EVENT_NAME"
+  
+  aws configure set aws_access_key_id "$WASABI_ACCESS_KEY_ID" --profile wasabi
+  aws configure set aws_secret_access_key "$WASABI_SECRET_ACCESS_KEY" --profile wasabi
 
   if [[ "$BUCKET" == *"prod"* ]]; then
     echo "using prod wasabi bucket"
@@ -17,16 +25,12 @@ function handler () {
     echo "using dev wasabi bucket"
     WASABI_BUCKET="dev-cdn.bken.io"
   fi
-
-  echo "WASABI_BUCKET: $WASABI_BUCKET"
-  echo "WASABI_ACCESS_KEY_ID: $WASABI_ACCESS_KEY_ID"
-  echo "WASABI_SECRET_ACCESS_KEY: $WASABI_SECRET_ACCESS_KEY"
   
   if [ "$EVENT_NAME" = "ObjectCreated:Put" ]; then
     echo "copying to wasabi"
     aws s3 cp s3://$BUCKET/$KEY - | \
-    AWS_ACCESS_KEY_ID=$WASABI_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$WASABI_SECRET_ACCESS_KEY \
-    aws s3 cp - s3://${WASABI_BUCKET}/${KEY} --endpoint=https://us-east-2.wasabisys.com
+    aws s3 cp - s3://${WASABI_BUCKET}/v/${VIDEO_ID}/${FILENAME} \
+    --endpoint=https://us-east-2.wasabisys.com --profile wasabi
   else
     echo "event did not match"
   fi
