40f0bbccd067d050df071f202993514917fa3672
Brendan Kennedy <brenwken@gmail.com>
2020-05-26 19:30:32 -0400

    Remux local audio instead of signed urls

--- modules/concat_server/concat.sh
+++ modules/concat_server/concat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eux
 
 IN_PATH="$(echo $1)"
 OUT_PATH="$(echo $2)"
@@ -24,7 +24,7 @@ mkdir -p ${TMP_DIR}/segments
 touch ${TMP_DIR}/manifest.txt
 
 echo "downloading segments"
-aws s3 sync $IN_PATH $TMP_DIR/segments
+aws s3 sync $IN_PATH $TMP_DIR/segments --quiet
 
 for SEGMENT in $(ls ${TMP_DIR}/segments); do
   echo "file 'segments/${SEGMENT}'" >> ${TMP_DIR}/manifest.txt
@@ -33,32 +33,35 @@ done
 FILE_EXT="${OUT_PATH##*.}"
 echo "FILE_EXT: ${FILE_EXT}"
 
-echo "LOCAL_VIDEO_PATH: $LOCAL_VIDEO_PATH"
 LOCAL_VIDEO_PATH="${TMP_DIR}/${PRESET_NAME}.${FILE_EXT}"
+echo "LOCAL_VIDEO_PATH: $LOCAL_VIDEO_PATH"
 
 if [ "$FILE_EXT" = "webm" ]; then
-  echo "creating signed audio url"
-  SIGNED_AUDIO_URL=$(aws s3 presign s3://${BUCKET}/audio/${VIDEO_ID}/source.ogg)
   FFMPEG_FORMAT="webm"
+  AUDIO_FILENAME="source.ogg"
 else
-  echo "creating signed audio url"
-  SIGNED_AUDIO_URL=$(aws s3 presign s3://${BUCKET}/audio/${VIDEO_ID}/source.aac)
   FFMPEG_FORMAT="mp4"
+  AUDIO_FILENAME="source.aac"
 fi
 
-echo "concatinating started"
+echo "downloading audio"
+aws s3 cp s3://${BUCKET}/audio/${VIDEO_ID}/${AUDIO_FILENAME} ${TMP_DIR}/${AUDIO_FILENAME} --quiet
+
+echo "concatinating segments"
 ffmpeg -f concat -safe 0 \
-  -protocol_whitelist "file,http,https,tcp,tls" \
   -i ${TMP_DIR}/manifest.txt \
-  -c copy \
-  -f matroska - | \
-  ffmpeg \
-  -i - -i "$SIGNED_AUDIO_URL" \
+  -c:v copy \
+  ${TMP_DIR}/premux.mkv
+
+echo "remuxing with audio"
+ffmpeg -i $TMP_DIR/premux.mkv \
+  -i ${TMP_DIR}/${AUDIO_FILENAME} \
   -c:v copy \
   -movflags faststart \
   $LOCAL_VIDEO_PATH
 
-aws s3 cp $LOCAL_VIDEO_PATH $OUT_PATH
+echo "uploading to s3"
+aws s3 cp $LOCAL_VIDEO_PATH $OUT_PATH --quiet
 
 rm -rf $TMP_DIR
 echo "concatinating completed"
