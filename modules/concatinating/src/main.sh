278994ddb2d336e8c02ad403539d3f4f809bbffc
Brendan Kennedy <brenwken@gmail.com>
2020-05-29 04:26:17 -0400

    Serverless concat

--- modules/concatinating/src/main.sh
+++ modules/concatinating/src/main.sh
@@ -5,7 +5,12 @@ function handler () {
   IN_PATH="$(echo $SQS_BODY | jq -r '.in_path')"
   OUT_PATH="$(echo $SQS_BODY | jq -r '.out_path')"
 
-  echo "invoking concatination server"
-  curl -X GET "http://172.31.33.233:4000/concat?in_path=${IN_PATH}&out_path=${OUT_PATH}"
-  echo "concatination completed"
+  # If the transcoded segments are larger than 400mb, we ask fargate to concat
+  # else we concat locally on lambda
+
+  ./concat.sh $IN_PATH $OUT_PATH
+
+  # echo "invoking concatination server"
+  # curl -X GET "http://172.31.33.233:4000/concat?in_path=${IN_PATH}&out_path=${OUT_PATH}"
+  # echo "concatination completed"
 }
