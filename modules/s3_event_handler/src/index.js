cd18080e8db850ed70ec6d45f7896d77aab8f781
Brendan Kennedy <brenwken@gmail.com>
2020-06-25 01:38:48 -0400

    Update audio scripts

--- modules/s3_event_handler/src/index.js
+++ modules/s3_event_handler/src/index.js
@@ -9,9 +9,10 @@ module.exports.handler = async function (event) {
 
   for (const record of event.Records) {
     const bucket = record.s3.bucket.name;
-    const [, id, preset, segment] = record.s3.object.key.split('/');
+    const folder = record.s3.object.key.split('/')[0];
 
-    if (preset === 'source') {
+    if (folder === 'audio') {
+      console.log('audio file uploaded');
       const { Items } = await db
         .query({
           TableName: TIDAL_TABLE,
@@ -21,31 +22,65 @@ module.exports.handler = async function (event) {
         .promise();
 
       await Promise.all(
-        Items.map(({ cmd, preset }) => {
-          // TODO :: Use environment variable once nomad servers are tracked in tf
-          const nomadAddr = '172.31.87.53/v1/job/transcoding/dispatch';
-          return axios.post(nomadAddr, {
-            Meta: {
-              cmd,
-              s3_in: `s3://${bucket}/segments/${id}/source/${segment}`,
-              s3_out: `s3://${bucket}/segments/${id}/${preset}/${segment}`,
-            },
-          });
+        Items.map(({ id, preset }) => {
+          return db
+            .update({
+              Key: { id, preset },
+              TableName: TIDAL_TABLE,
+              UpdateExpression: 'SET #audio.#audioExt = :path',
+              ExpressionAttributeValues: {
+                ':path': { S: record.s3.object.key },
+              },
+              ExpressionAttributeNames: {
+                // TODO :: Probably unreliable way of getting extension
+                '#audioExt': record.s3.object.key.split('.')[1],
+                '#audio': 'audio',
+              },
+            })
+            .promise();
         })
       );
-    } else {
-      await db
-        .update({
-          Key: { id, preset },
-          TableName: TIDAL_TABLE,
-          UpdateExpression: 'SET #segments.#segName = :status',
-          ExpressionAttributeValues: { ':status': { BOOL: true } },
-          ExpressionAttributeNames: {
-            '#segName': segment,
-            '#segments': 'segments',
-          },
-        })
-        .promise();
+    }
+
+    if (folder === 'segments') {
+      const [, id, preset, segment] = record.s3.object.key.split('/');
+
+      if (preset === 'source') {
+        const { Items } = await db
+          .query({
+            TableName: TIDAL_TABLE,
+            KeyConditionExpression: 'id = :id',
+            ExpressionAttributeValues: { ':id': id },
+          })
+          .promise();
+
+        await Promise.all(
+          Items.map(({ cmd, preset }) => {
+            // TODO :: Use environment variable once nomad servers are tracked in tf
+            const nomadAddr = '172.31.87.53/v1/job/transcoding/dispatch';
+            return axios.post(nomadAddr, {
+              Meta: {
+                cmd,
+                s3_in: `s3://${bucket}/segments/${id}/source/${segment}`,
+                s3_out: `s3://${bucket}/segments/${id}/${preset}/${segment}`,
+              },
+            });
+          })
+        );
+      } else {
+        await db
+          .update({
+            Key: { id, preset },
+            TableName: TIDAL_TABLE,
+            UpdateExpression: 'SET #segments.#segName = :status',
+            ExpressionAttributeValues: { ':status': { BOOL: true } },
+            ExpressionAttributeNames: {
+              '#segName': segment,
+              '#segments': 'segments',
+            },
+          })
+          .promise();
+      }
     }
   }
 };
