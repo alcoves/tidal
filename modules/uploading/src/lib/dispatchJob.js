af545c7eeb889d586b26c14eadbd1275d8a50d88
Brendan Kennedy <brenwken@gmail.com>
2020-06-25 14:26:32 -0400

    Increase request timeout

--- modules/uploading/src/lib/dispatchJob.js
+++ modules/uploading/src/lib/dispatchJob.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = function (job, Meta) {
   const nomadAddr = `http://172.31.29.153:4646/v1/job/${job}/dispatch`;
   return axios
-    .post(nomadAddr, { Meta }, { timeout: 10 })
+    .post(nomadAddr, { Meta }, { timeout: 1000 * 10 })
     .then((res) => {
       console.log(res);
     })
