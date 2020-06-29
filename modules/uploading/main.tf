7419036089e5ce0731d11cd125f6fa89df4aa019
Brendan Kennedy <brenwken@gmail.com>
2020-06-29 18:20:25 -0400

    Better bucket interpolation

--- modules/uploading/main.tf
+++ modules/uploading/main.tf
@@ -36,7 +36,7 @@ resource "aws_lambda_function" "tidal_uploading" {
     variables = {
       FFMPEG_PATH   = "/opt/ffmpeg/ffmpeg"
       FFPROBE_PATH  = "/opt/ffmpeg/ffprobe"
-      WASABI_BUCKET = "${var.env}-cdn.bken.io"
+      WASABI_BUCKET = "${var.env == "dev" ? "dev-" : ""}cdn.bken.io"
     }
   }
 }
