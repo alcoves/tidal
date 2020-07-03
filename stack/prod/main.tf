7dac6a7928aedfec9c07a69e5c5b40b06d76cf16
Brendan Kennedy <brenwken@gmail.com>
2020-07-02 20:54:58 -0400

    us-east-2

--- stack/prod/main.tf
+++ stack/prod/main.tf
@@ -2,31 +2,37 @@ terraform {
   backend "s3" {
     region = "us-east-1"
     bucket = "bken-tf-state"
-    key    = "services/tidal/dev/tidal.tfstate"
+    key    = "services/tidal/prod/tidal.tfstate"
   }
 }
 
-provider "aws" { region = "us-east-1" }
+provider "aws" { region = "us-east-2" }
 
 variable "env" {
   type    = string
   default = "prod"
 }
 
-module "core" {
-  env       = var.env
-  namespace = "bken"
-  source    = "../../modules/core"
-}
-
 module "uploading" {
-  env               = var.env
-  source            = "../../modules/uploading"
-  uploads_queue_arn = module.core.uploads_queue_arn
+  env    = var.env
+  source = "../../modules/uploading"
 }
 
-module "pipeline" {
-  env                   = var.env
-  source                = "../../modules/segmenting"
-  app_image             = "594206825329.dkr.ecr.us-east-1.amazonaws.com/tidal:dev"
+module "s3_event_handler" {
+  env    = var.env
+  source = "../../modules/s3_event_handler"
+}
+
+module "db_event_handler" {
+  env                 = var.env
+  tidal_db_stream_arn = module.core.tidal_db_stream_arn
+  source              = "../../modules/db_event_handler"
+}
+
+module "core" {
+  env                           = var.env
+  namespace                     = "bken"
+  source                        = "../../modules/core"
+  uploading_function_arn        = module.uploading.function_arn
+  s3_event_handler_function_arn = module.s3_event_handler.function_arn
 }
