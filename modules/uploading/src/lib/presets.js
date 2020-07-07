6d48e9bf7fb9a98cacf51d8d333765cb8582ba25
Brendan Kennedy <brenwken@gmail.com>
2020-07-07 11:39:47 -0400

    Create x264 factory preset function

--- modules/uploading/src/lib/presets.js
+++ modules/uploading/src/lib/presets.js
@@ -1,20 +1,60 @@
-const presets = require('./presets.json');
+function x264(framerate, width) {
+  if (!framerate || !width) {
+    throw new Error('framerate and width must be defined');
+  }
 
-module.exports = (presetName, framerate) => {
+  const commands = [
+    "-c:v libx264",
+    "-preset faster",
+    "-profile:v high",
+    "-bf 3",
+    "-g 60",
+    "-keyint_min 60",
+    "-crf 24",
+    "-coder 1",
+    "-pix_fmt yuv420p",
+    `-vf fps=fps=${framerate},scale=${width}:-2`
+  ]
+
+  return commands.join(' ')
+}
+
+module.exports = function (presetName, framerate) {
   if (!presetName || !framerate) {
     throw new Error('preset and framerate must be defined');
   }
 
-  const preset = presets[presetName];
-
-  // Constant frame rate mode
-  preset.cmd['-vf'] = `fps=${framerate},${preset.cmd['-vf']}`;
-
-  const commands = Object.entries(preset.cmd).map(([k, v]) => {
-    return `${k} ${v}`
-  })
-
-  preset.cmd = commands.join(' ');
-  console.log('command', preset.cmd);
-  return preset;
+  if (presetName === 'libx264-480p') {
+    return {
+      ext: 'mp4',
+      preset: 'libx264-480p',
+      cmd: x264(framerate, 854)
+    }
+  } else if (presetName === 'libx264-720p') {
+    return {
+      ext: 'mp4',
+      preset: 'libx264-720p',
+      cmd: x264(framerate, 1280)
+    }
+  } else if (presetName === 'libx264-1080p') {
+    return {
+      ext: 'mp4',
+      preset: 'libx264-1080p',
+      cmd: x264(framerate, 1920)
+    }
+  } else if (presetName === 'libx264-1440p') {
+    return {
+      ext: 'mp4',
+      preset: 'libx264-1440p',
+      cmd: x264(framerate, 2360)
+    }
+  } else if (presetName === 'libx264-2160p') {
+    return {
+      ext: 'mp4',
+      preset: 'libx264-2160p',
+      cmd: x264(framerate, 3840)
+    }
+  } else {
+    throw new Error(`preset: ${presetName} not found`);
+  }
 }
