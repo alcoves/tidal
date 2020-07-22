9367e750dc169614dea7c8cbe039448ad17fdcb9
Brendan Kennedy <brenwken@gmail.com>
2020-07-22 03:13:56 -0400

    Remove range key from tidal db

--- modules/core/db.tf
+++ modules/core/db.tf
@@ -1,18 +1,12 @@
 resource "aws_dynamodb_table" "tidal_db" {
   stream_enabled   = true
   hash_key         = "id"
-  range_key        = "preset"
   billing_mode     = "PAY_PER_REQUEST"
   name             = "tidal-${var.env}"
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
-    type = "S"
-  }
-
-  attribute {
-    name = "preset"
     type = "S"
   }
 }
