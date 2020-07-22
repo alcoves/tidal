3fe41a43eca5691391bbf20fe83945c29bc0a227
Brendan Kennedy <brenwken@gmail.com>
2020-07-22 09:15:20 -0400

    Disable concatinating

--- modules/db_event_handler/src/index.js
+++ modules/db_event_handler/src/index.js
@@ -10,44 +10,44 @@ module.exports.handler = async ({ Records }) => {
     const item = AWS.DynamoDB.Converter.unmarshall(event.dynamodb.NewImage);
 
     if (item.status === 'segmenting') {
-      const transcoded = Object.values(item.segments).reduce((acc, cv) => {
-        if (cv) acc++;
-        return acc;
-      }, 0);
+      //   const transcoded = Object.values(item.segments).reduce((acc, cv) => {
+      //     if (cv) acc++;
+      //     return acc;
+      //   }, 0);
 
-      if (
-        item.segmentCount === transcoded &&
-        Object.keys(item.audio).length >= 2
-      ) {
-        const Meta = {
-          s3_in: `s3://${TIDAL_BUCKET}/segments/${item.id}/${item.preset}`,
-          s3_out: `s3://${CDN_BUCKET}/v/${item.id}/${item.preset}.${item.ext}`,
-        };
-        const nomadAddr = `http://10.0.3.87:4646/v1/job/concatinating/dispatch`;
+      //   if (
+      //     item.segmentCount === transcoded &&
+      //     Object.keys(item.audio).length >= 2
+      //   ) {
+      //     const Meta = {
+      //       s3_in: `s3://${TIDAL_BUCKET}/segments/${item.id}/${item.preset}`,
+      //       s3_out: `s3://${CDN_BUCKET}/v/${item.id}/${item.preset}.${item.ext}`,
+      //     };
+      //     const nomadAddr = `http://10.0.3.87:4646/v1/job/concatinating/dispatch`;
 
-        const { Item } = await db
-          .get({
-            TableName: 'config',
-            Key: { id: 'NOMAD_TOKEN' },
-          })
-          .promise();
+      //     const { Item } = await db
+      //       .get({
+      //         TableName: 'config',
+      //         Key: { id: 'NOMAD_TOKEN' },
+      //       })
+      //       .promise();
 
-        await axios
-          .post(
-            nomadAddr,
-            { Meta },
-            {
-              headers: {
-                'X-Nomad-Token': Item.value,
-              },
-              timeout: 1000 * 10,
-            }
-          )
-          .catch((error) => {
-            console.error(error);
-            throw error;
-          });
-      }
+      //     await axios
+      //       .post(
+      //         nomadAddr,
+      //         { Meta },
+      //         {
+      //           headers: {
+      //             'X-Nomad-Token': Item.value,
+      //           },
+      //           timeout: 1000 * 10,
+      //         }
+      //       )
+      //       .catch((error) => {
+      //         console.error(error);
+      //         throw error;
+      //       });
+      //   }
     }
   }
 };
