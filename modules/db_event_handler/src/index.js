c827bee18ace28bae3dbac6864fb1dee0dbc5f8f
Brendan Kennedy <brenwken@gmail.com>
2020-07-22 23:17:30 -0400

    Use old image to determine concatination strategy

--- modules/db_event_handler/src/index.js
+++ modules/db_event_handler/src/index.js
@@ -1,20 +1,32 @@
 const AWS = require('aws-sdk');
 const dispatchJob = require('./lib/dispatchJob');
-const obtainDbLock = require('./lib/obtainDbLock');
+// const obtainDbLock = require('./lib/obtainDbLock');
 
 const { CDN_BUCKET, TIDAL_BUCKET } = process.env;
 
 module.exports.handler = async ({ Records }) => {
   for (const event of Records) {
     console.log(JSON.stringify(event));
-    const video = AWS.DynamoDB.Converter.unmarshall(event.dynamodb.NewImage);
+    const oldVideo = AWS.DynamoDB.Converter.unmarshall(event.dynamodb.OldImage);
+    const newVideo = AWS.DynamoDB.Converter.unmarshall(event.dynamodb.NewImage);
 
-    for (const version of Object.values(video.versions)) {
-      if ((version.status === 'segmenting') && (version.segmentsCompleted === video.segmentCount) && (Object.keys(video.audio).length >= 2)) {
-        if (await obtainDbLock(version.preset)) {
+    if (Object.keys(oldVideo).length && Object.keys(newVideo).length) {
+      const versions = Object.values(newVideo.versions)
+
+      for (const version of versions) {
+        const [oldVersion] = Object.values(oldVideo.versions).filter((old) => {
+          if (old.preset === version.preset) {
+            return true;
+          }
+        })
+
+        console.log(version.preset, oldVersion.segmentsCompleted === newVideo.segmentCount - 1, version.segmentsCompleted === newVideo.segmentCount);
+
+        if ((oldVersion.segmentsCompleted === newVideo.segmentCount - 1) && (version.segmentsCompleted === newVideo.segmentCount)) {
+          console.log(`concatinating ${version.preset}`);
           await dispatchJob('concatinating', {
-            s3_in: `s3://${TIDAL_BUCKET}/segments/${video.id}/${version.preset}`,
-            s3_out: `s3://${CDN_BUCKET}/v/${video.id}/${version.preset}.${video.ext}`,
+            s3_in: `s3://${TIDAL_BUCKET}/segments/${newVideo.id}/${version.preset}`,
+            s3_out: `s3://${CDN_BUCKET}/v/${newVideo.id}/${version.preset}.${newVideo.ext}`,
           });
         }
       }
