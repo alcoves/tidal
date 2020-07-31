01bc61858af7ebf076b9e3541b4f2ca470a3db30
Brendan Kennedy <brenwken@gmail.com>
2020-07-31 17:35:07 -0400

    Gracefully handle concatination error

--- modules/db_event_handler/src/lib/handleConcatination.js
+++ modules/db_event_handler/src/lib/handleConcatination.js
@@ -5,44 +5,46 @@ const { CDN_BUCKET, TIDAL_BUCKET, TIDAL_TABLE } = process.env;
 const db = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
 
 module.exports = async function (oldImage, newImage) {
-  const versions = Object.values(newImage.versions);
+  if (newImage.versions) {
+    const versions = Object.values(newImage.versions);
 
-  for (const version of versions) {
-    const [oldVersion] = Object.values(oldImage.versions).filter((old) => {
-      if (old.preset === version.preset) {
-        return true;
-      }
-    });
-
-    console.log(
-      version.preset,
-      oldVersion.segmentsCompleted === newImage.segmentCount - 1,
-      version.segmentsCompleted === newImage.segmentCount
-    );
-
-    if (
-      oldVersion.segmentsCompleted === newImage.segmentCount - 1 &&
-      version.segmentsCompleted === newImage.segmentCount
-    ) {
-      console.log(`concatinating ${version.preset}`);
-      await dispatchJob('concatinating', {
-        s3_in: `s3://${TIDAL_BUCKET}/segments/${newImage.id}/${version.preset}`,
-        s3_out: `s3://${CDN_BUCKET}/v/${newImage.id}/${version.preset}.${version.ext}`,
+    for (const version of versions) {
+      const [oldVersion] = Object.values(oldImage.versions).filter((old) => {
+        if (old.preset === version.preset) {
+          return true;
+        }
       });
 
-      await db
-        .update({
-          Key: { id: newImage.id },
-          TableName: TIDAL_TABLE,
-          UpdateExpression: 'set #versions.#preset.#status = :concatinating',
-          ExpressionAttributeValues: { ':concatinating': 'concatinating' },
-          ExpressionAttributeNames: {
-            '#status': 'status',
-            '#versions': 'versions',
-            '#preset': version.preset,
-          },
-        })
-        .promise();
+      console.log(
+        version.preset,
+        oldVersion.segmentsCompleted === newImage.segmentCount - 1,
+        version.segmentsCompleted === newImage.segmentCount
+      );
+
+      if (
+        oldVersion.segmentsCompleted === newImage.segmentCount - 1 &&
+        version.segmentsCompleted === newImage.segmentCount
+      ) {
+        console.log(`concatinating ${version.preset}`);
+        await dispatchJob('concatinating', {
+          s3_in: `s3://${TIDAL_BUCKET}/segments/${newImage.id}/${version.preset}`,
+          s3_out: `s3://${CDN_BUCKET}/v/${newImage.id}/${version.preset}.${version.ext}`,
+        });
+
+        await db
+          .update({
+            Key: { id: newImage.id },
+            TableName: TIDAL_TABLE,
+            UpdateExpression: 'set #versions.#preset.#status = :concatinating',
+            ExpressionAttributeValues: { ':concatinating': 'concatinating' },
+            ExpressionAttributeNames: {
+              '#status': 'status',
+              '#versions': 'versions',
+              '#preset': version.preset,
+            },
+          })
+          .promise();
+      }
     }
   }
 };
