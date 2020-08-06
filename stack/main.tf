1f09218af59a6f4e95ac0a4d2251c68591f9b9c8
Brendan Kennedy <brenwken@gmail.com>
2020-08-05 20:20:35 -0400

    Fix module path

--- stack/main.tf
+++ stack/main.tf
@@ -15,18 +15,18 @@ variable "env" {
 
 module "uploading" {
   env    = var.env
-  source = "../../modules/uploading"
+  source = "../modules/uploading"
 }
 
 module "s3_event_handler" {
   env    = var.env
-  source = "../../modules/s3_event_handler"
+  source = "../modules/s3_event_handler"
 }
 
 module "db_event_handler" {
   env                 = var.env
   tidal_db_stream_arn = module.core.tidal_db_stream_arn
-  source              = "../../modules/db_event_handler"
+  source              = "../modules/db_event_handler"
 }
 
 module "core" {
@@ -36,7 +36,7 @@ module "core" {
   subnet_1                      = "bken_1_a"
   subnet_2                      = "bken_1_b"
   subnet_3                      = "bken_1_c"
-  source                        = "../../modules/core"
+  source                        = "../modules/core"
   uploading_function_arn        = module.uploading.function_arn
   s3_event_handler_function_arn = module.s3_event_handler.function_arn
 }
