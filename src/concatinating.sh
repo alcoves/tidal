d1851083c1b1ee3177ca18351aeca85132949a28
Brendan Kennedy <brenwken@gmail.com>
2020-09-10 16:04:26 -0400

    Add distributed concatination locking system

---
+++ src/concatinating.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+IN_PATH=$1
+OUT_PATH=$2
+
+BUCKET="$(echo $IN_PATH | cut -d'/' -f3)"
+echo "BUCKET: ${BUCKET}"
+
+TIDAL_ENV="$(echo $BUCKET | cut -d'-' -f3)"
+echo "TIDAL_ENV: ${TIDAL_ENV}"
+
+VIDEO_ID="$(echo $IN_PATH | cut -d'/' -f5)"
+echo "VIDEO_ID: ${VIDEO_ID}"
+
+PRESET_NAME="$(echo $IN_PATH | cut -d'/' -f6)"
+echo "PRESET_NAME: ${PRESET_NAME}"
+
+WASABI_BUCKET="$(echo $OUT_PATH | cut -d'/' -f3)"
+echo "WASABI_BUCKET: ${WASABI_BUCKET}"
+
+VIDEO_EXTENSION="${OUT_PATH##*.}"
+echo "VIDEO_EXTENSION: ${VIDEO_EXTENSION}"
+
+echo "creating tmp dir"
+TMP_DIR=$(mktemp -d)
+mkdir $TMP_DIR/audio
+mkdir $TMP_DIR/segments
+echo "TMP_DIR: $TMP_DIR"
+
+TMP_VIDEO_PATH="${TMP_DIR}/${PRESET_NAME}.${VIDEO_EXTENSION}"
+echo "TMP_VIDEO_PATH: $TMP_VIDEO_PATH"
+
+echo "creating manifest"
+MANIFEST=${TMP_DIR}/manifest.txt
+touch $MANIFEST
+
+echo "download segments"
+aws s3 sync $IN_PATH $TMP_DIR/segments
+
+for SEGMENT in $(ls $TMP_DIR/segments); do
+  echo "file '${TMP_DIR}/segments/${SEGMENT}'" >> $MANIFEST
+done
+
+if [ "$VIDEO_EXTENSION" = "webm" ]; then
+  AUDIO_EXT="ogg"
+else 
+  AUDIO_EXT="aac"
+fi
+
+AUDIO_PATH="${TMP_DIR}/audio/source.${AUDIO_EXT}"
+
+echo "AUDIO_EXT: $AUDIO_EXT"
+echo "AUDIO_PATH: $AUDIO_PATH"
+
+echo "downloading audio"
+aws s3 cp s3://${BUCKET}/audio/${VIDEO_ID}/source.${AUDIO_EXT} $AUDIO_PATH
+
+echo "concatinating started"
+# -hide_banner -loglevel panic
+# NOTE :: audio will be processed here, this could be improved
+ffmpeg -y -f concat -safe 0 \
+  -i $MANIFEST \
+  -c copy \
+  -f matroska - | \
+  ffmpeg \
+  -y -i - -i $AUDIO_PATH \
+  -c:v copy \
+  -movflags faststart \
+  $TMP_VIDEO_PATH
+
+echo "copying to wasabi"
+aws s3 mv $TMP_VIDEO_PATH $OUT_PATH \
+  --profile wasabi \
+  --content-type "video/$VIDEO_EXTENSION" \
+  --endpoint=https://us-east-2.wasabisys.com
+
+CDN_PROTOCAL="https"
+LINK="${OUT_PATH/s3/$CDN_PROTOCAL}"
+echo "LINK: $LINK"
+
+echo "updating tidal database with status"
+aws dynamodb update-item \
+  --table-name "tidal-${TIDAL_ENV}" \
+  --key '{"id": {"S": '\"$VIDEO_ID\"'}}' \
+  --expression-attribute-values '{":status":{"S":"completed"},":link":{"S":'\"$LINK\"'}}' \
+  --update-expression 'SET #versions.#preset.#status = :status, #versions.#preset.#link = :link' \
+  --expression-attribute-names '{"#versions":"versions","#status":"status","#link":"link","#preset":'\"$PRESET_NAME\"'}'
+
+echo "removing tmp dir"
+rm -rf $TMP_DIR
--- src/transcoding.sh
+++ src/transcoding.sh
@@ -13,10 +13,12 @@ echo "creating signed source url"
 SIGNED_SOURCE_URL=$(aws s3 presign $S3_IN --profile digitalocean --endpoint=https://nyc3.digitaloceanspaces.com)
 
 echo "parsing variables"
+BUCKET="$(echo $S3_OUT | cut -d'/' -f3)"  
 VIDEO_ID="$(echo $S3_OUT | cut -d'/' -f5)"  
 PRESET_NAME="$(echo $S3_OUT | cut -d'/' -f6)"
 SEGMENT_NAME="$(echo $S3_OUT | cut -d'/' -f7)"
 
+echo "BUCKET: ${BUCKET}"
 echo "VIDEO_ID: ${VIDEO_ID}"
 echo "PRESET_NAME: ${PRESET_NAME}"
 echo "SEGMENT_NAME: ${SEGMENT_NAME}"
@@ -29,5 +31,31 @@ ffmpeg -y -i "$SIGNED_SOURCE_URL" $FFMPEG_COMMAND $TMP_VIDEO_PATH
 
 echo "moving transcode to s3"
 aws s3 mv $TMP_VIDEO_PATH $S3_OUT --profile digitalocean --endpoint=https://nyc3.digitaloceanspaces.com
+
+echo "counting source segments"
+SOURCE_SEGMENTS_COUNT=$(aws s3 ls s3://${BUCKET}/segments/${VIDEO_ID}/source/ --profile digitalocean --endpoint=https://nyc3.digitaloceanspaces.com | wc -l)
+
+echo "counting transcoded segments"
+TRANSCODED_SEGMENTS_COUNT=$(aws s3 ls s3://${BUCKET}/segments/${VIDEO_ID}/${PRESET_NAME}/ --profile digitalocean --endpoint=https://nyc3.digitaloceanspaces.com | wc -l)
+
+if ["$SOURCE_SEGMENTS_COUNT" -eq "$TRANSCODED_SEGMENTS_COUNT"]; then
+  function concat {
+    EXISTS=$(consul kv get tidal/${VIDEO_ID}/${PRESET_NAME})
+    if [ -z "$EXISTS" ]; then
+      echo "dispatching concatination job"
+      nomad job dispatch \
+        -detach \
+        -meta cmd="$CMD" \
+        -meta s3_in="s3://${BUCKET}/segments/${VIDEO_ID}/source/${SEGMENT}" \
+        -meta s3_out="s3://${BUCKET}/segments/${VIDEO_ID}/${PRESET_NAME}/${SEGMENT}" \
+        concatinating
+      consul kv delete "tidal/${VIDEO_ID}/${PRESET_NAME}"
+    else
+      echo "aquired concatiantion lock, but had already been handled"
+    fi
+  }
+
+  consul lock "tidal/${VIDEO_ID}" concat()
+fi
 
 echo "done!"
