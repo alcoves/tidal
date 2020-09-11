df009ea523b34c5ce192b483e29ecbeb40fdf45d
Brendan Kennedy <brenwken@gmail.com>
2020-09-10 20:23:57 -0400

    Add audio

--- src/concatinating.sh
+++ src/concatinating.sh
@@ -18,7 +18,7 @@ echo "VIDEO_EXTENSION: ${VIDEO_EXTENSION}"
 
 echo "creating tmp dir"
 TMP_DIR=$(mktemp -d)
-# mkdir $TMP_DIR/audio
+mkdir $TMP_DIR/audio
 mkdir $TMP_DIR/segments
 echo "TMP_DIR: $TMP_DIR"
 
@@ -30,36 +30,35 @@ MANIFEST=${TMP_DIR}/manifest.txt
 touch $MANIFEST
 
 echo "download segments"
-aws s3 sync $IN_PATH $TMP_DIR/segments --profile digitalocean --endpoint=https://nyc3.digitaloceanspaces.com
+aws s3 sync \
+  $IN_PATH \
+  $TMP_DIR/segments \
+  --profile digitalocean \
+  --endpoint=https://nyc3.digitaloceanspaces.com
 
 for SEGMENT in $(ls $TMP_DIR/segments); do
   echo "file '${TMP_DIR}/segments/${SEGMENT}'" >> $MANIFEST
 done
 
-# if [ "$VIDEO_EXTENSION" = "webm" ]; then
-#   AUDIO_EXT="ogg"
-# else 
-#   AUDIO_EXT="aac"
-# fi
+AUDIO_PATH="${TMP_DIR}/audio/source.wav"
+echo "AUDIO_PATH: $AUDIO_PATH"
 
-# AUDIO_PATH="${TMP_DIR}/audio/source.${AUDIO_EXT}"
-
-# echo "AUDIO_EXT: $AUDIO_EXT"
-# echo "AUDIO_PATH: $AUDIO_PATH"
-
-# echo "downloading audio"
-# aws s3 cp s3://${BUCKET}/audio/${VIDEO_ID}/source.${AUDIO_EXT} $AUDIO_PATH
+echo "downloading audio"
+aws s3 cp \
+  s3://${BUCKET}/audio/${VIDEO_ID}/source.${AUDIO_EXT} \
+  $AUDIO_PATH \ 
+  --profile digitalocean \ 
+  --endpoint=https://nyc3.digitaloceanspaces.com
 
 echo "concatinating started"
 # -hide_banner -loglevel panic
-# NOTE :: audio will be processed here, this could be improved
-# -i $AUDIO_PATH \
 ffmpeg -y -f concat -safe 0 \
   -i $MANIFEST \
   -c copy \
   -f matroska - | \
   ffmpeg \
   -y -i - \
+  -i $AUDIO_PATH \
   -c:v copy \
   -movflags faststart \
   $TMP_VIDEO_PATH
@@ -69,10 +68,6 @@ aws s3 mv $TMP_VIDEO_PATH $OUT_PATH \
   --profile wasabi \
   --content-type "video/$VIDEO_EXTENSION" \
   --endpoint=https://us-east-2.wasabisys.com
-
-# CDN_PROTOCAL="https"
-# LINK="${OUT_PATH/s3/$CDN_PROTOCAL}"
-# echo "LINK: $LINK"
 
 echo "removing tmp dir"
 rm -rf $TMP_DIR
