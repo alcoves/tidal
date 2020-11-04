5bcd55240078e0242c9d3fea177b958142a8bf7f
Brendan Kennedy <brenwken@gmail.com>
2020-11-03 23:11:43 -0500

    Add input

--- src/concatinating.sh
+++ src/concatinating.sh
@@ -78,6 +78,7 @@ rm -f ${TMP_HLS_PATH}/master.m3u8
 echo "creating concatinated video file"
 CONCAT_VIDEO_PATH=$(mktemp --suffix=.ts)
 ffmpeg -hide_banner -y \
+  -i $MANIFEST \
   -f concat \
   -safe 0 \
   -c copy \
