3c12f2f4b2c3ac72b29b506ecbed61fc603132ce
Brendan Kennedy <brenwken@gmail.com>
2020-11-10 21:20:11 -0500

    Fix mime type

--- src/thumbnail.sh
+++ src/thumbnail.sh
@@ -33,8 +33,7 @@ echo "calling ffmpeg"
 ffmpeg -y -i "$SIGNED_URL" $CMD $THUMB_PATH
 
 echo "get thumbnail mime type"
-# MIME_TYPE=$(file --mime-type $THUMB_PATH)
-MIME_TYPE="image/webp"
+MIME_TYPE=$(file -b --mime-type $THUMB_PATH)
 
 echo "copying to wasabi"
 aws s3 mv $THUMB_PATH $OUT_PATH \
