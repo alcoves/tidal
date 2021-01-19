fbffc08d0e854275afec8341c0310746c5768010
Brendan Kennedy <brenwken@gmail.com>
2021-01-19 00:41:32 -0500

    Serial manifest creation

--- src/package.sh
+++ src/package.sh
@@ -85,6 +85,19 @@ ffmpeg -hide_banner -y \
   -hls_segment_filename "$HLS_DIR/%09d.m4s" \
   $HLS_DIR/stream.m3u8
 
+RESOLUTION=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $MUXED_VIDEO_PATH)
+BITRATE=$(ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 $MUXED_VIDEO_PATH)
+
+echo "adding comments to m3u8 playlist file"
+echo "# Created By: https://github.com/bkenio/tidal"
+echo "# STREAM-INF:BANDWIDTH=$BITRATE,RESOLUTION=$RESOLUTION,NAME=$PRESET_NAME" >> $HLS_DIR/stream.m3u8
+
+echo "moving $MUXED_VIDEO_PATH to cdn"
+aws s3 cp $MUXED_VIDEO_PATH s3://cdn.bken.io/v/${VIDEO_ID}/progressive/ \
+  --quiet \
+  --profile wasabi \
+  --endpoint=https://us-east-2.wasabisys.com
+
 echo "publishing hls assets to cdn"
 aws s3 cp $HLS_DIR s3://cdn.bken.io/v/${VIDEO_ID}/hls/$PRESET_NAME \
   --quiet \
@@ -92,53 +105,19 @@ aws s3 cp $HLS_DIR s3://cdn.bken.io/v/${VIDEO_ID}/hls/$PRESET_NAME \
   --profile wasabi \
   --endpoint=https://us-east-2.wasabisys.com
 
-RESOLUTION=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $MUXED_VIDEO_PATH)
-BITRATE=$(ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 $MUXED_VIDEO_PATH)
+echo "ready for master manifest creation, aquiring lock"
+# Always ensure that this lock is unique to a single video
+export LOCK_KEY="tidal/${VIDEO_ID}/hls/master.m3u8"
 
-HLS_MASTER="$TMP_DIR/master.m3u8"
-REMOTE_MASTER_PATH="s3://cdn.bken.io/v/${VIDEO_ID}/hls/master.m3u8"
-MASTER_PATH_COUNT=$(aws s3 ls $REMOTE_MASTER_PATH --profile wasabi --endpoint=https://us-east-2.wasabisys.com | wc -l)
+# Make these variables availible to the child process
+export BITRATE=$BITRATE
+export VIDEO_ID=$VIDEO_ID
+export RESOLUTION=$RESOLUTION
+export PRESET_NAME=$PRESET_NAME
 
-if [ "$MASTER_PATH_COUNT" -gt 0 ]; then
-  echo "remote master preset exists, downloading"
-  aws s3 cp $REMOTE_MASTER_PATH $HLS_MASTER \
-    --quiet \
-    --profile wasabi \
-    --endpoint=https://us-east-2.wasabisys.com
-
-  if grep -q "NAME=$PRESET_NAME" "$HLS_MASTER"; then
-    echo "current preset was alredy found in the remote master"
-  else
-    echo "current preset was not found in remote master, adding"
-
-    echo "#EXT-X-STREAM-INF:BANDWIDTH=$BITRATE,RESOLUTION=$RESOLUTION,NAME=$PRESET_NAME" >> $HLS_MASTER
-    echo "$PRESET_NAME/stream.m3u8" >> $HLS_MASTER
-
-    echo "uploading $HLS_MASTER"
-    aws s3 cp $HLS_MASTER s3://cdn.bken.io/v/${VIDEO_ID}/hls/master.m3u8 \
-      --quiet \
-      --profile wasabi \
-      --endpoint=https://us-east-2.wasabisys.com
-  fi
-else
-  echo "remote master not found, creating"
-
-  echo "#EXTM3U" >> $HLS_MASTER
-  echo "#EXT-X-STREAM-INF:BANDWIDTH=$BITRATE,RESOLUTION=$RESOLUTION,NAME=$PRESET_NAME" >> $HLS_MASTER
-  echo "$PRESET_NAME/stream.m3u8" >> $HLS_MASTER
-
-  echo "uploading $HLS_MASTER"
-  aws s3 cp $HLS_MASTER s3://cdn.bken.io/v/${VIDEO_ID}/hls/master.m3u8 \
-    --quiet \
-    --profile wasabi \
-    --endpoint=https://us-east-2.wasabisys.com
-fi
-
-echo "moving $MUXED_VIDEO_PATH to cdn"
-aws s3 cp $MUXED_VIDEO_PATH s3://cdn.bken.io/v/${VIDEO_ID}/progressive/ \
-  --quiet \
-  --profile wasabi \
-  --endpoint=https://us-east-2.wasabisys.com
+# This command will wait until a lock is aquired
+# When a lock is aquired, the master.m3u8 will be created by this process
+consul lock $LOCK_KEY $TIDAL_PATH/src/services/lockPackage.sh
 
 echo "removing $TMP_DIR"
 rm -rf $TMP_DIR
