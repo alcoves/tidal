446b99c43e1b0d8730f71cf4aa092355df8956a6
Brendan Kennedy <brenwken@gmail.com>
2021-01-24 03:20:08 -0500

    🎉 Thumbnail creation with go cli

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/bken-io/tidal/src/services"
+	"github.com/bken-io/tidal/src/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -23,10 +24,26 @@ func main() {
 		Short: "Generate video thumbnail",
 		Args:  cobra.MinimumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
-			inputPath := strings.Join(args, " ")
 			ffmpegCmd, _ := cmd.Flags().GetString("cmd")
-			awsProfile, _ := cmd.Flags().GetString("profile")
-			services.GenerateThumbnail(services.ThumbnailInput)
+			endpoint, _ := cmd.Flags().GetString("endpoint")
+			accessKeyID, _ := cmd.Flags().GetString("accessKeyId")
+			secretAccessKey, _ := cmd.Flags().GetString("secretAccessKey")
+
+			if ffmpegCmd == "" {
+				ffmpegCmd = "-vf scale=854:480:force_original_aspect_ratio=increase,crop=854:480 -vframes 1 -q:v 50"
+			}
+
+			event := utils.ThumbnailInput{
+				S3In:  args[0],
+				S3Out: args[1],
+				Cmd:   ffmpegCmd,
+				S3Config: utils.S3Config{
+					Endpoint:        endpoint,
+					AccessKeyID:     accessKeyID,
+					SecretAccessKey: secretAccessKey,
+				},
+			}
+			services.GenerateThumbnail(event)
 		},
 	}
 
@@ -35,5 +52,8 @@ func main() {
 	rootCmd.AddCommand(thumbnailCmd)
 
 	thumbnailCmd.Flags().String("cmd", "", "ffmpeg thumbnail command")
+	thumbnailCmd.Flags().String("endpoint", "", "the aws endpoint to use")
+	thumbnailCmd.Flags().String("accessKeyId", "", "the accessKeyId to use")
+	thumbnailCmd.Flags().String("secretAccessKey", "", "the secretAccessKey to use")
 	rootCmd.Execute()
 }
