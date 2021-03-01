d160f0e0515e162192c70c8144f46b009385fe00
Brendan Kennedy <brenwken@gmail.com>
2021-02-28 23:21:05 -0500

    Remove legacy stuff

--- legacy/preview.go
+++
@@ -1,4 +0,0 @@
-package commands
-
-// TODO :: Implement
-// ffmpeg -i winter.mp4 -c:v libwebp -compression_level 6 -q:v 60 -preset default -loop 0 -an -vsync 0 -t 5 -vf scale=480:-2,fps=fps=12 out.webp
