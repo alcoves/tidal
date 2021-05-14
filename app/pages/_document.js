ffd9fa1be029cb018002a416b338aaaa5b3d13a8
Brendan Kennedy <brenwken@gmail.com>
2021-05-14 08:53:42 -0400

    Dark theme toggle

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import theme from '../styles/theme';
+import { ColorModeScript, } from '@chakra-ui/react';
 import Document, { Html, Head, Main, NextScript, } from 'next/document';
 
 export default class MyDocument extends Document {
@@ -8,8 +10,11 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="description" content="User interface for tidal" />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap' rel='stylesheet' />
         </Head>
         <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
