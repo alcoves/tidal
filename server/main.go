222d5ac36cba1a2893a0b15fdf9533e12f6806a8
Brendan Kennedy <brenwken@gmail.com>
2021-05-21 10:36:42 -0400

    Routes

--- server/main.go
+++ server/main.go
@@ -9,5 +9,54 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	app.Get("/config", func(c *fiber.Ctx) error {
+		return c.SendString("Here is the config")
+	})
+
+	app.Patch("/config", func(c *fiber.Ctx) error {
+		return c.SendString("modyfying the config")
+	})
+
+	app.Post("/jobs", func(c *fiber.Ctx) error {
+		return c.SendString("creating a new one shot job")
+	})
+	
+	app.Get("/jobs", func(c *fiber.Ctx) error {
+		return c.SendString("list jobs")
+	})
+
+	app.Get("/jobs/:id", func(c *fiber.Ctx) error {
+		return c.SendString("getting job information")
+	})
+
+	app.Post("/jobs/:id/segment", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/audio", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/thumbnail", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/:id/transcode/:segment ", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/:id/:preset/concatinate", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/:id/package", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+	app.Post("/jobs/:id/package", func(c *fiber.Ctx) error {
+		return c.SendString("segmenting")
+	})
+
+
 	app.Listen(":3000")
 }
