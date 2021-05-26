2ec17caa1e0117db938f7f7e437b634f5a79fd99
Brendan Kennedy <brenwken@gmail.com>
2021-05-26 17:25:24 -0400

    Generate presets

---
+++ utils/types.go
@@ -0,0 +1,37 @@
+package utils
+
+// Preset is a struct containing transcoder commands
+type Preset struct {
+	Name    string `json:"name"`
+	Command string `json:"command"`
+}
+
+// Response is what goes back to the caller
+type Response struct {
+	Presets []Preset `json:"presets"`
+}
+
+// VideoMetadata is a slim ffprobe struct
+type VideoMetadata struct {
+	Width     int     `json:"width"`
+	Height    int     `json:"height"`
+	Bitrate   int     `json:"bitrate"`
+	Rotate    int     `json:"rotate"`
+	Framerate float64 `json:"framerate"`
+	Duration  float64 `json:"duration"`
+	HasAudio  bool    `json:"hasAudio"`
+}
+
+// Config is a global tidal configuration
+type Config struct {
+	TidalDir   string   `json:"tidalDir"`
+	RcloneEnvs []string `json:"rcloneEnvs"`
+}
+
+// Job is used to create a new video encoding job
+type Job struct {
+	ID              string        `json:"id"`
+	Metadata        VideoMetadata `json:"metadata"`
+	Presets         []Preset      `json:"presets"`
+	RcloneSourceURI string        `json:"rcloneSourceUri"`
+}
