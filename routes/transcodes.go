bfa98da09f3ff6caaf38bedaae5f98d486dc5778
Brendan Kennedy <brenwken@gmail.com>
2021-06-04 16:33:18 -0400

    202 all the way

--- routes/transcodes.go
+++ routes/transcodes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"github.com/bkenio/tidal/cmd"
 	"github.com/bkenio/tidal/utils"
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,17 +10,11 @@ func PostTranscode(c *fiber.Ctx) error {
 	if err := c.BodyParser(job); err != nil {
 		return err
 	}
-
-	if job.Async {
-		utils.Dispatch("transcode", map[string]string{
-			"video_id":               job.VideoID,
-			"webhook_url":            job.WebhookURL,
-			"rclone_source_uri":      job.RcloneSourceURI,
-			"rclone_destination_uri": job.RcloneDestinationURI,
-		})
-		return c.SendStatus(202)
-	} else {
-		cmd.Transcode(job)
-		return c.SendStatus(200)
-	}
+	utils.Dispatch("transcode", map[string]string{
+		"video_id":               job.VideoID,
+		"webhook_url":            job.WebhookURL,
+		"rclone_source_uri":      job.RcloneSourceURI,
+		"rclone_destination_uri": job.RcloneDestinationURI,
+	})
+	return c.SendStatus(202)
 }
