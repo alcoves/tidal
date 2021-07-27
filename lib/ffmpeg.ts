6f6046482ea5e503f66ead4d75efc73df8431ad5
Brendan Kennedy <brenwken@gmail.com>
2021-07-26 23:35:07 -0400

    Add utility functions

---
+++ lib/ffmpeg.ts
@@ -0,0 +1,35 @@
+import { spawn } from 'child_process';
+
+const ffmpegPath = '/usr/local/bin/ffmpeg';
+
+export default function ffmpeg(inPath: string, outPath: string) {
+  const args = [
+    '-y', '-hide_banner', 
+    '-i', inPath,
+    '-s', '640x480', 
+    '-codec:a', 'aac', 
+    '-b:a', '44.1k', 
+    '-r', '15', 
+    '-b:v', '1000k', 
+    '-c:v','h264', 
+    '-f', 'mp4', outPath
+  ];
+
+  const proc = spawn(ffmpegPath, args);
+
+  return new Promise((resolve, reject) => {
+    proc.stdout.on('data', function(data) {
+      console.log(data);
+    });
+
+    proc.stderr.setEncoding("utf8")
+    proc.stderr.on('data', function(data) {
+      console.log('stderr data', data);
+    });
+
+    proc.on('close', function() {
+      console.log('finished');
+      resolve('done');
+    });
+  })
+}
---
+++ pages/api/status.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  nomad: boolean,
+  consul: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'GET') {
+    res.status(200).json({
+      nomad: true,
+      consul: true
+    });
+  }
+  res.status(405).end();
+}
