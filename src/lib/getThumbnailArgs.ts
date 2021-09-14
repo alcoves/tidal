58dda7e0328a7b7fad61c23348a55d70d93199aa
Brendan Kennedy <brenwken@gmail.com>
2021-09-14 10:39:18 -0400

    Up the thumbnail quality

--- src/lib/getThumbnailArgs.ts
+++ src/lib/getThumbnailArgs.ts
@@ -1,6 +1,6 @@
 export default function getThumbnailArgs(): string[] {
   return [
     "-vf", "scale=854:480:force_original_aspect_ratio=increase,crop=854:480",
-    "-vframes", "1", "-q:v", "75", "-f", "image2"
+    "-vframes", "1", "-q:v", "80", "-f", "image2"
   ]
 }
