bc4f8c4cec210094998fc4f0c7679ed25965591f
Brendan Kennedy <brenwken@gmail.com>
2021-09-23 02:33:52 -0400

    Sorting

--- lib/generateManifest.ts
+++ lib/generateManifest.ts
@@ -1,13 +1,11 @@
 import { AssetInterface } from "../models/models"
 
-// TODO :: Sort renditions such that the 720p is loaded first
-// See mux.com manifests as an example
 export function generateManifest(asset: AssetInterface): string {
   let manifest = `#EXTM3U
 #EXT-X-VERSION:5
 #EXT-X-INDEPENDENT-SEGMENTS
   `
-  for (const r of asset.renditions) {
+  for (const r of asset.renditions.sort((a, b) => (a.width < b.width) ? 1 : -1)) {
     if (r.status === "completed") {
       manifest += `
 #EXT-X-STREAM-INF:BANDWIDTH=${r.bandwidth},AVERAGE-BANDWIDTH=${r.bandwidth},CODECS="${r.codecs}",RESOLUTION=${r.width}x${r.height},CLOSED-CAPTIONS=NONE
