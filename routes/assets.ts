e8226921cce9ad5b0e549cb068c796400e4ad8c2
Brendan Kennedy <brenwken@gmail.com>
2021-09-23 03:47:49 -0400

    Create thumbnails on upload

--- routes/assets.ts
+++ routes/assets.ts
@@ -1,11 +1,12 @@
 import express from "express"
-import { createAsset, fetchAsset, fetchAssetManifest } from "../controllers/assets"
+import { createAsset, deleteAsset, fetchAsset, fetchAssetManifest } from "../controllers/assets"
 
 const router = express.Router()
 
 // Ordering here matters!
 router.get("/:assetId.m3u8", fetchAssetManifest)
 router.get("/:assetId", fetchAsset)
+router.delete("/:assetId", deleteAsset)
 
 router.post("/", createAsset)
 
