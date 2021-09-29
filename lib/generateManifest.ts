92af927cd7fd879b602f6bdeeaa64360d37df241
Brendan Kennedy <brenwken@gmail.com>
2021-09-29 02:07:12 -0400

    More HLS flags

--- lib/generateManifest.ts
+++ lib/generateManifest.ts
@@ -4,14 +4,29 @@ export function generateManifest(asset: AssetInterface): string {
   let manifest = `#EXTM3U
 #EXT-X-VERSION:5
 #EXT-X-INDEPENDENT-SEGMENTS
-  `
-  for (const r of asset.renditions.sort((a, b) => (a.width < b.width) ? 1 : -1)) {
-    if (r.status === "completed") {
+`
+  
+  const completedRenditions = asset.renditions.filter((r) => r.status === "completed")
+  if (completedRenditions.length) {
+    // All renditions are completed
+    for (const r of completedRenditions.sort((a, b) => (a.width < b.width) ? 1 : -1)) {
       manifest += `
 #EXT-X-STREAM-INF:BANDWIDTH=${r.bandwidth},AVERAGE-BANDWIDTH=${r.bandwidth},CODECS="${r.codecs}",RESOLUTION=${r.width}x${r.height},CLOSED-CAPTIONS=NONE
 ${process.env.CDN_URL}/v/${asset._id}/${r._id}/stream.m3u8
-          `
+`
     }
   }
+  // const livestreamingRenditions = asset.renditions.filter((r) => r.status === "running")
+  //   if (livestreamingRenditions.length) {
+  //     // There is no completed rendition so but we can still return livestreams
+  //     for (const r of livestreamingRenditions.sort((a, b) => (a.width < b.width) ? 1 : -1)) {
+  //       manifest += `
+  // #EXT-X-STREAM-INF:BANDWIDTH=${r.bandwidth},AVERAGE-BANDWIDTH=${r.bandwidth},CODECS="${r.codecs}",RESOLUTION=${r.width}x${r.height},CLOSED-CAPTIONS=NONE
+  // ${process.env.CDN_URL}/v/${asset._id}/${r._id}/stream.m3u8
+  // `
+  //       break
+  //     }
+  //   }
+
   return manifest
 }
