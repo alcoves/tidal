89f12aa1eb872bff8ff7c7c459bbae1938a59928
rustyguts <9222118+rustyguts@users.noreply.github.com>
2021-12-30 21:30:05 -0500

    Add config page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,24 @@
 import NextLink from 'next/link'
 import { GoDashboard } from 'react-icons/go'
-import { Button, Flex } from '@chakra-ui/react'
+import { IoSettingsOutline } from 'react-icons/io5'
+import { Button, Flex, VStack } from '@chakra-ui/react'
 
 export default function Layout({ children }: any) {
   return (
     <Flex h='100vh' w='100%'>
       <Flex direction='column' w='200px' color='white' bg='gray.900'>
-        <Flex justify='center' align='center' p='4'>
+        <VStack spacing={2} p='4'>
           <NextLink href='/' passHref>
-            <Button w='100%' leftIcon={<GoDashboard />}>
+            <Button w='100%' justifyContent='start' leftIcon={<GoDashboard />}>
               Dashboard
             </Button>
           </NextLink>
-        </Flex>
+          <NextLink href='/config' passHref>
+            <Button w='100%' justifyContent='start' leftIcon={<IoSettingsOutline />}>
+              Config
+            </Button>
+          </NextLink>
+        </VStack>
       </Flex>
       <Flex w='100%' overflowY='auto'>
         {children}
---
+++ pages/config.tsx
@@ -0,0 +1,10 @@
+import Config from '../components/Config'
+import Layout from '../components/Layout'
+
+export default function ConfigPage() {
+  return (
+    <Layout>
+      <Config />
+    </Layout>
+  )
+}
