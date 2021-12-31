fe6b75bec136ef52b39ff3449aba51aa95c1acaa
rustyguts <9222118+rustyguts@users.noreply.github.com>
2021-12-30 21:30:20 -0500

    Add job processor scaffold

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+async function processJobs() {
+  setInterval(() => {
+    // console.log('Processing Jobs')
+  }, 500)
+}
+
+processJobs()
+
 module.exports = {
   reactStrictMode: true,
 }
