e195b6494d6d283994ee95cdeed8ade96b60f5de
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-01-03 15:38:04 -0500

    Add Joi input validation

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -1,20 +1,49 @@
+import Joi from 'joi'
 import { transcodeQueue } from '../config/queues/transcode'
 import { thumbnailQueue } from '../config/queues/thumbnail'
 import { metadataQueue, metadataWorker } from '../config/queues/metadata'
 
 export async function transcodeController(req, res) {
-  const { input, output } = req.body
-  await transcodeQueue.add('transcode', {
-    input,
-    output,
-    ffmpeg_command: '-c:v libx265 -crf 23',
+  const schema = Joi.object({
+    input: Joi.object({
+      bucket: Joi.string().required().max(255),
+      key: Joi.string().required().max(255),
+    }),
+    output: Joi.object({
+      bucket: Joi.string().required().max(255),
+      key: Joi.string().required().max(255),
+    }),
   })
+
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  })
+
+  if (error) return res.status(400).json(error)
+
+  await transcodeQueue.add('transcode', value)
   return res.sendStatus(202)
 }
 
 export async function metadataController(req, res) {
-  const { input } = req.body
-  const { id } = await metadataQueue.add('metadata', { input })
+  const schema = Joi.object({
+    input: Joi.object({
+      bucket: Joi.string().required().max(255),
+      key: Joi.string().required().max(255),
+    }),
+  })
+
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  })
+
+  if (error) return res.status(400).json(error)
+
+  const { id } = await metadataQueue.add('metadata', value)
   metadataWorker.on('completed', function (job) {
     if (job.id === id) {
       return res.json(job.data)
@@ -28,10 +57,25 @@ export async function metadataController(req, res) {
 }
 
 export async function thumbnailController(req, res) {
-  const { input, output } = req.body
-  await thumbnailQueue.add('thumbnail', {
-    input,
-    output,
+  const schema = Joi.object({
+    input: Joi.object({
+      bucket: Joi.string().required().max(255),
+      key: Joi.string().required().max(255),
+    }),
+    output: Joi.object({
+      bucket: Joi.string().required().max(255),
+      key: Joi.string().required().max(255),
+    }),
   })
+
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  })
+
+  if (error) return res.status(400).json(error)
+
+  await thumbnailQueue.add('thumbnail', value)
   return res.sendStatus(202)
 }
