977c820e4975e25b20eb091701dd0c387d5acfd5
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-04-12 16:21:33 -0400

    Remove logs

--- ui/src/components/Queues/CleanQueues.tsx
+++ ui/src/components/Queues/CleanQueues.tsx
@@ -2,9 +2,9 @@ import { Button } from '@chakra-ui/react'
 import { useLazyRequest } from '../../hooks/useRequest'
 
 export default function CleanQueues() {
-  const [cleanQueue, { loading, data, error }] = useLazyRequest('/queues/clean', { method: 'POST' })
+  const [cleanQueue, { loading, error }] = useLazyRequest('/queues/clean', { method: 'POST' })
+  if (error) alert(error)
 
-  console.log(loading, data, error)
   return (
     <Button isLoading={loading} onClick={() => cleanQueue()}>
       Clean Queues
