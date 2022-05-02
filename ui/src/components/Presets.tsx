ecb79313a512cc22572852d2cbe0f37e4896085d
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2022-05-02 13:28:13 -0400

    Preset page layout

--- ui/src/components/Presets.tsx
+++ ui/src/components/Presets.tsx
@@ -1,9 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading, HStack } from '@chakra-ui/react'
+import CreatePreset from './Presets/CreatePreset'
 
 export default function Presets() {
   return (
     <Box>
-      <Box>Here are some Presets</Box>
+      <Box mb='2'>
+        <Heading mb='2'>Presets</Heading>
+        <HStack justify='end'>
+          <CreatePreset />
+        </HStack>
+      </Box>
     </Box>
   )
 }
