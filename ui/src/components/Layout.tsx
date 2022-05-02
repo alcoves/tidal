65d629cfa58ff820bd1f12432dd8eeb6ef89983b
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2022-05-02 13:27:49 -0400

    Overflow spacing

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -3,15 +3,15 @@ import { Box, Flex, Heading } from '@chakra-ui/react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Box>
+    <Box overflow='hidden'>
       <Flex p='2' h='50px' align='center' bg='yellow.500'>
         <Heading size='md' color='gray.800'>
           Tidal UI
         </Heading>
       </Flex>
-      <Flex>
+      <Flex h='calc(100vh - 50px)'>
         <Sidebar />
-        <Box p='2' w='100%'>
+        <Box overflowY='auto' p='2' w='100%'>
           {children}
         </Box>
       </Flex>
