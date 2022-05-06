51c11e075e45de16a95778a5c3eaa9bba8356a4e
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-05-05 23:57:59 -0400

    Create rendition UI

---
+++ ui/src/components/Renditions/CreateRendition.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import { useLazyRequest } from '../../hooks/useRequest'
+import { Button, HStack, Input } from '@chakra-ui/react'
+
+export default function CreateRendition({ mutate }: { mutate: any }) {
+  const [createRendition, { loading, error }] = useLazyRequest('/renditions', { method: 'POST' })
+  const [rendition, setRendition] = useState({
+    name: '',
+    cmd: '',
+  })
+
+  async function handleCreate() {
+    await createRendition({ data: rendition })
+  }
+
+  useEffect(() => {
+    if (!loading && !error) {
+      setRendition({ name: '', cmd: '' })
+      mutate()
+    }
+  }, [loading, error])
+
+  function handleInputChange(e) {
+    setRendition({ ...rendition, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <HStack>
+      <Input
+        w='auto'
+        size='sm'
+        name='name'
+        variant='filled'
+        isDisabled={loading}
+        value={rendition.name}
+        onChange={handleInputChange}
+        placeholder='Rendition Name'
+      />
+      <Input
+        w='100%'
+        size='sm'
+        name='cmd'
+        variant='filled'
+        isDisabled={loading}
+        value={rendition.cmd}
+        onChange={handleInputChange}
+        placeholder='FFmpeg Command'
+      />
+      <Button isLoading={loading} colorScheme='yellow' size='sm' onClick={handleCreate}>
+        Create
+      </Button>
+    </HStack>
+  )
+}
