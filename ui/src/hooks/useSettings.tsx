100d893d716494e75753e81ce3e06869c176897f
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-06-17 12:04:45 -0400

    Seperate UI

--- ui/src/hooks/useSettings.tsx
+++
@@ -1,7 +0,0 @@
-import useSWR from 'swr'
-import { fetcher } from '../utils/fetcher'
-
-export function useSettings() {
-  const { data, error } = useSWR(`/settings`, fetcher)
-  return { settings: data, error }
-}
