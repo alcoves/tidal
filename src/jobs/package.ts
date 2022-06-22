4c1be5bf10dd7da3ffa78bc572203f5b6e5f2ad8
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-06-22 17:22:06 -0400

    Package with rclone

--- src/jobs/package.ts
+++ src/jobs/package.ts
@@ -1,20 +1,20 @@
 import fs from 'fs-extra'
-import Path from 'path'
 import { PackageJob } from '../types'
-import { amazonS3URI, getSignedURL, uploadFile, uploadFolder } from '../config/s3'
-import { downloadFile, execProm } from '../utils/utils'
+import { rclone } from '../lib/rclone'
+import { execProm } from '../utils/utils'
 
 export async function packageJob(job: PackageJob) {
-  console.log('Package job starting...')
+  console.log('package job starting...')
   const { inputs, output } = job.data
   const tmpDir = await fs.mkdtemp('/tmp/tidal-package-')
 
   try {
-    for (const { path } of inputs) {
-      const signedURL = await getSignedURL(amazonS3URI(path))
-      const filename = Path.basename(path)
-      await downloadFile(signedURL, `${tmpDir}/${filename}`)
-    }
+    console.info('downloading inputs')
+    await Promise.all(
+      inputs.map(({ path }) => {
+        return rclone(`copy ${path} ${tmpDir}`)
+      })
+    )
 
     const packageCommands = [
       'packager',
@@ -27,18 +27,8 @@ export async function packageJob(job: PackageJob) {
     const result = await execProm(packageCommands.join(' '), tmpDir)
     console.log('Package Result: ', result)
 
-    console.log('Cleaning up downloads')
-    // Removes the files used for packaging that we don't want in the remote
-    // Could also move packaging assets to a seperate folder
-    const files = await fs.readdir(tmpDir)
-    await Promise.all(
-      files.map(f => {
-        if (f.includes('.mp4')) return fs.rm(`${tmpDir}/${f}`)
-      })
-    )
-
-    console.info(`Uploading package to ${output}`)
-    await uploadFolder(tmpDir, amazonS3URI(output))
+    console.info(`copying ${tmpDir} to ${output}`)
+    await rclone(`copy ${tmpDir} ${output}`)
   } catch (error) {
     console.error(error)
   } finally {
