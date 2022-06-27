4140e7dbd7b4ae1cbff3ae009c9e9cc2ee25d7fe
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-06-27 13:02:39 -0400

    Add thumbnail jobs args

--- src/controllers/assets.ts
+++ src/controllers/assets.ts
@@ -5,7 +5,29 @@ import { ImportAssetData, ThumbnailJobData } from '../types'
 
 export async function createAssetThumbnail(req, res) {
   const { assetId } = req.params
-  const thumbnailJob: ThumbnailJobData = { assetId }
+
+  const schema = Joi.object({
+    output: Joi.string().uri().required(),
+    width: Joi.number().min(1).max(10000).required(),
+    height: Joi.number().min(1).max(10000).required(),
+    fit: Joi.string().uri().default('cover').valid('cover', 'contain', 'fill', 'inside', 'outside'),
+  })
+
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  })
+  if (error) return res.status(400).json(error)
+
+  const thumbnailJob: ThumbnailJobData = {
+    assetId,
+    fit: value.fit,
+    width: value.width,
+    height: value.height,
+    output: value.output.replace('$assetId', assetId).replace('$id', uuidv4()),
+  }
+
   const queue = getQueueByName('thumbnail')
   if (queue) await queue.queue.add('thumbnail', thumbnailJob)
   return res.sendStatus(202)
