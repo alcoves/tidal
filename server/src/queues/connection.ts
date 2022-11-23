f19096669886c9e76c6cda4053a87e089f6aff7f
Brendan Kennedy <brenwken@gmail.com>
2022-11-23 06:31:39 +0000

    More error handling on redis connection

--- server/src/queues/connection.ts
+++ server/src/queues/connection.ts
@@ -1,7 +1,14 @@
+import chalk from 'chalk'
 import { ConnectionOptions } from 'bullmq'
 
+if (!process.env.REDIS_PORT) process.exit(1)
+if (!process.env.REDIS_HOST) process.exit(1)
+if (!process.env.REDIS_PASSWORD) process.exit(1)
+
+console.log(chalk.blue(`Trying to connect to ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`))
+
 export const connection: ConnectionOptions = {
+  host: process.env.REDIS_HOST,
   password: process.env.REDIS_PASSWORD,
-  host: process.env.REDIS_HOST || 'localhost',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
+  port: parseInt(process.env.REDIS_PORT),
 }
