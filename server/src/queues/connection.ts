d06e1faa92db578a76a27f00c036ca3fcc954144
Brendan Kennedy <brenwken@gmail.com>
2022-11-23 17:06:58 +0000

    Redis is working with docker compose

--- server/src/queues/connection.ts
+++ server/src/queues/connection.ts
@@ -5,10 +5,11 @@ if (!process.env.REDIS_PORT) process.exit(1)
 if (!process.env.REDIS_HOST) process.exit(1)
 if (!process.env.REDIS_PASSWORD) process.exit(1)
 
-console.log(chalk.blue(`Trying to connect to ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`))
+const msg = `Trying to connect to ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+console.log(chalk.blue(msg))
 
 export const connection: ConnectionOptions = {
   host: process.env.REDIS_HOST,
   password: process.env.REDIS_PASSWORD,
-  port: parseInt(process.env.REDIS_PORT),
+  port: Number(process.env.REDIS_PORT),
 }
