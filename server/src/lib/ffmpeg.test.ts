a0c082b96106b92a86e45fbfa1ee2ff10091ca11
Brendan Kennedy <brenwken@gmail.com>
2022-12-14 07:38:45 +0000

    Fix build

--- server/src/lib/ffmpeg.test.ts
+++ server/src/lib/ffmpeg.test.ts
@@ -9,7 +9,7 @@ describe('ffprobe', () => {
 
   test('it returns metadata in the json format using cwd arg', async () => {
     const commands = '-v quiet -print_format json -show_format -show_streams test.mp4'
-    const res = await ffprobe(commands, { cwd: './data' })
+    const res = await ffprobe(commands)
     expect(res).toMatchSnapshot()
   })
 })
